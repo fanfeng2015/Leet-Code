a5326eacabc96ae351a5de5fbb1d29eb81e69518
Fan Feng <fanfeng2015@gmail.com>
2018-08-06 12:59:41 -0700

    LeetCode #165

--- src/CompareVersionNumbers.java
+++ src/CompareVersionNumbers.java
@@ -9,7 +9,7 @@ public class CompareVersionNumbers {
 		String[] sequence2 = version2.split("\\.");
 		int length = Math.max(sequence1.length, sequence2.length);
 		for (int i = 0; i < length; i++) {
-			Integer num1 = i < sequence1.length ? Integer.parseInt(sequence1[i]) : 1;
+			Integer num1 = i < sequence1.length ? Integer.parseInt(sequence1[i]) : 0;
 			Integer num2 = i < sequence2.length ? Integer.parseInt(sequence2[i]) : 0;
 			int compare = num1.compareTo(num2);
 			if (compare != 0) {
