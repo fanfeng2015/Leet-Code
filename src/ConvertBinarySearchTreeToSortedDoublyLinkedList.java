99384e4275316891fba97b966998f16beb3194ea
Fan Feng <fanfeng2015@gmail.com>
2018-08-06 17:39:40 -0700

    LeetCode #426

---
+++ src/ConvertBinarySearchTreeToSortedDoublyLinkedList.java
@@ -0,0 +1,48 @@
+// LeetCode #426 (Convert Binary Search Tree to Sorted Doubly Linked List).
+
+// Convert a BST to a sorted circular doubly-linked list in-place. Think of the left
+// and right pointers as synonymous to the previous and next pointers in a doubly-linked 
+// list.
+
+public class ConvertBinarySearchTreeToSortedDoublyLinkedList {
+
+	// recursive in-order
+	TreeNode prev = null;
+
+	public TreeNode treeToDoublyList(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+		TreeNode head = new TreeNode(0);
+		prev = head;
+		inorder(root);
+		// connect head and tail
+		prev.right = head.right;
+		head.right.left = prev;
+		return head.right;
+	}
+
+	private void inorder(TreeNode cur) {
+		if (cur == null) {
+			return;
+		}
+		inorder(cur.left);
+		prev.right = cur;
+		cur.left = prev;
+		prev = cur;
+		inorder(cur.right);
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(n).
+
+	private static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
