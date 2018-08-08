86dc24472b22547c6f3231680c491e7e8ef84f7d
Fan Feng <fanfeng2015@gmail.com>
2018-08-08 14:56:33 -0700

    LeetCode #123

--- src/BestTimeToBuyAndSellStock3.java
+++ src/BestTimeToBuyAndSellStock3.java
@@ -9,24 +9,24 @@
 
 public class BestTimeToBuyAndSellStock3 {
 
-	public int maxProfit(int[] array) {
+	public int maxProfit(int[] prices) {
 		int buyOne = Integer.MIN_VALUE, buyTwo = Integer.MIN_VALUE;
 		int sellOne = 0, sellTwo = 0;
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < prices.length; i++) {
 			// If this is the first stock I buy, and if I buy on day i, do I save money than
 			// if I buy on any previous day?
 			// If yes, then I buy on day i. Otherwise, I buy on some previous day, whichever
 			// is the cheapest.
-			buyOne = Math.max(buyOne, -array[i]);
+			buyOne = Math.max(buyOne, -prices[i]);
 			// If this is the first stock I sell, and if I sell on day i, do I make more
 			// money than if I sell on any previous day?
 			// If yes, then I sell on day i. Otherwise, I sell on some previous day,
 			// whichever gives me the most profit.
-			sellOne = Math.max(sellOne, buyOne + array[i]);
+			sellOne = Math.max(sellOne, buyOne + prices[i]);
 			// Now that I have bought and sold the first stock, I have sellOne money at
 			// hand, instead of 0.
-			buyTwo = Math.max(buyTwo, sellOne - array[i]);
-			sellTwo = Math.max(sellTwo, buyTwo + array[i]);
+			buyTwo = Math.max(buyTwo, sellOne - prices[i]);
+			sellTwo = Math.max(sellTwo, buyTwo + prices[i]);
 		}
 		return sellTwo;
 	}
