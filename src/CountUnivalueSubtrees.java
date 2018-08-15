7bc4ce961e44950696c41787b5bbf5e0e5943e3c
Fan Feng <fanfeng2015@gmail.com>
2018-08-15 16:24:05 -0700

    LeetCode #250

---
+++ src/CountUnivalueSubtrees.java
@@ -0,0 +1,43 @@
+// LeetCode #250 (Count Univalue Subtrees).
+
+// Given a binary tree, count the number of uni-value subtrees.
+
+// A Uni-value subtree means all nodes of the subtree have the same value.
+
+public class CountUnivalueSubtrees {
+
+	private int count = 0;
+
+	public int countUnivalSubtrees(TreeNode root) {
+		isUnivalSubtree(root);
+		return count;
+	}
+
+	private boolean isUnivalSubtree(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		boolean left = isUnivalSubtree(root.left);
+		boolean right = isUnivalSubtree(root.right);
+		if (left && right && sameAsChild(root)) {
+			count++;
+			return true;
+		}
+		return false;
+	}
+
+	private boolean sameAsChild(TreeNode root) {
+		return (root.left == null || root.val == root.left.val) && (root.right == null || root.val == root.right.val);
+	}
+
+	private static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+}
