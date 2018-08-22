053a84d426213993ca58606eac1e9591c0bdfa36
Fan Feng <fanfeng2015@gmail.com>
2018-08-22 17:09:57 -0400

    LeetCode #535

--- src/EncodeAndDecodeTinyURL.java
+++ src/EncodeAndDecodeTinyURL.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.HashMap;
+
 // LeetCode #535 (Encode and Decode TinyURL).
 
 // TinyURL is a URL shortening service where you enter a URL such as 
@@ -11,14 +14,35 @@
 
 public class EncodeAndDecodeTinyURL {
 
+	private static final int ENCODING_LENGTH = 6;
+	private static final String BASE_HOST = "http://tinyurl.com/";
+	private static final String SEED = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-,";
+
+	private Map<String, String> shortToLong = new HashMap<>();
+	private Map<String, String> longToShort = new HashMap<>();
+
 	public String encode(String longUrl) {
-		return "";
+		if (longToShort.containsKey(longUrl)) {
+			return BASE_HOST + longToShort.get(longUrl);
+		}
+		StringBuilder encoding = new StringBuilder();
+		do {
+			encoding.setLength(0);
+			encoding.trimToSize();
+			for (int i = 0; i < ENCODING_LENGTH; i++) {
+				int index = (int) (Math.random() * SEED.length());
+				encoding.append(SEED.charAt(index));
+			}
+		} while (shortToLong.containsKey(encoding.toString()));
+
+		shortToLong.put(encoding.toString(), longUrl);
+		longToShort.put(longUrl, encoding.toString());
+
+		return BASE_HOST + encoding;
 	}
 
 	public String decode(String shortUrl) {
-		return "";
+		return shortToLong.get(shortUrl.replace(BASE_HOST, ""));
 	}
-	
-	// Time complexity is .
 
 }
