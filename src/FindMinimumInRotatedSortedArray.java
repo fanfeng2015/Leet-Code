e17e6ae662d9c123f353e69071dc824e5d72f23a
Fan Feng <fanfeng2015@gmail.com>
2018-08-24 13:56:09 -0400

    LeetCode #153

---
+++ src/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,35 @@
+
+// LeetCode #153 (Find Minimum in Rotated Sorted Array).
+
+// Suppose an array sorted in ascending order is rotated at some pivot
+// unknown to you beforehand.
+
+// For example, [0, 1, 2, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2].
+
+// Find the minimum element.
+
+// You may assume no duplicate exists in the array.
+
+public class FindMinimumInRotatedSortedArray {
+
+	public int findMin(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return -1;
+		}
+		int left = 0, right = nums.length - 1;
+		while (left < right - 1) {
+			int mid = left + (right - left) / 2;
+			if (nums[left] > nums[mid]) {
+				right = mid;
+			} else if (nums[mid] > nums[right]) {
+				left = mid;
+			} else {
+				return left;
+			}
+		}
+		return nums[left] <= nums[right] ? left : right;
+	}
+
+	// Time complexity is O(log(n)).
+	// Space complexity is O(1).
+}
