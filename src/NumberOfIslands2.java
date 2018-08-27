21ea0a4f9d6ced8b06f0352246135880213f1e6a
Fan Feng <fanfeng2015@gmail.com>
2018-08-27 13:27:36 -0400

    LeetCode #200

--- src/NumberOfIslands2.java
+++ src/NumberOfIslands2.java
@@ -14,18 +14,18 @@ import java.util.List;
 
 public class NumberOfIslands2 {
 
-	private int[][] directions = new int[][] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+	private static final int[][] DIRECTIONS = new int[][] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
 	public List<Integer> numIslands(int m, int n, int[][] positions) {
 		List<Integer> result = new ArrayList<>();
-		char[][] grid = new char[m][n]; // default char is fine
+		int[][] grid = new int[m][n]; // default int is 0
 		UnionFind unionFind = new UnionFind(grid);
 		for (int[] position : positions) {
 			int row = position[0];
 			int col = position[1];
-			grid[row][col] = '1'; // add land
+			grid[row][col] = 1; // add land
 			unionFind.add(row, col);
-			for (int[] direction : directions) {
+			for (int[] direction : DIRECTIONS) {
 				int newRow = row + direction[0];
 				int newCol = col + direction[1];
 				if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && grid[newRow][newCol] == '1') {
@@ -39,4 +39,80 @@ public class NumberOfIslands2 {
 
 	// Time complexity is O(k * log(m*n)).
 	// Space complexity is O(m*n).
+
+	private class UnionFind {
+
+		private int m;
+		private int n;
+		private int[] parent; // parent of i
+		private int[] size; // size of the tree rooted at i
+
+		private int count; // number of components
+
+		public UnionFind(int[][] grid) {
+			m = grid.length;
+			n = grid[0].length;
+			parent = new int[m * n];
+			size = new int[m * n];
+			// Note: There is no 1 in the grid initially.
+		}
+
+		public int count() {
+			return count;
+		}
+
+		// add land operation
+		public void add(int row, int col) {
+			int index = row * n + col;
+			parent[index] = index;
+			size[index] = 1;
+			count++;
+		}
+
+		public int find(int p) {
+			validate(p);
+			int root = p;
+			while (root != parent[root]) {
+				root = parent[root];
+			}
+			while (p != root) { // path compression to make tree flat
+				int newParent = parent[root];
+				parent[p] = root;
+				p = newParent;
+			}
+			return root;
+		}
+
+		public boolean connected(int p, int q) {
+			return find(p) == find(q);
+		}
+
+		public void union(int p, int q) {
+			int parentP = find(p);
+			int parentQ = find(q);
+			if (parentP == parentQ) {
+				return;
+			}
+			// weighted quick union by size: make smaller root point to larger root
+			if (size[parentP] < size[parentQ]) {
+				parent[parentP] = parentQ;
+				size[parentQ] += size[parentP];
+			} else {
+				parent[parentQ] = parentP;
+				size[parentP] += size[parentQ];
+			}
+			count--;
+		}
+
+		private void validate(int p) {
+			int n = parent.length;
+			if (p < 0 || p >= n) {
+				throw new IllegalArgumentException("Index " + p + " is not between 0 and " + (n - 1));
+			}
+		}
+
+		// Time complexity is O((m+n) * log(n)), on any m union-find operations
+		// on a set of n objects.
+	}
+
 }
