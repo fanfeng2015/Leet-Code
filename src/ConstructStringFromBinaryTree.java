84ec3276225c01cced296e2797b28f8259accc5f
Fan Feng <fanfeng2015@gmail.com>
2018-08-26 23:08:39 -0400

    LeetCode #606

--- src/ConstructStringFromBinaryTree.java
+++ src/ConstructStringFromBinaryTree.java
@@ -30,14 +30,4 @@ public class ConstructStringFromBinaryTree {
 
 	// Time complexity is O(n).
 	// Space complexity is O(n).
-
-	private class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-
-		TreeNode(int x) {
-			val = x;
-		}
-	}
 }
