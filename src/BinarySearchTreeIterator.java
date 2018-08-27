c2da6ffdd04891c305fdfbecb0ae04645e1b1302
Fan Feng <fanfeng2015@gmail.com>
2018-08-26 22:45:34 -0400

    LeetCode #173

--- src/BinarySearchTreeIterator.java
+++ src/BinarySearchTreeIterator.java
@@ -40,14 +40,4 @@ public class BinarySearchTreeIterator {
 		return result;
 	}
 
-	private class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-
-		TreeNode(int x) {
-			val = x;
-		}
-	}
-
 }
