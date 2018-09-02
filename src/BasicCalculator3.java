d95b94f56f682df60093fb8eac62e5266ae6522d
Fan Feng <fanfeng2015@gmail.com>
2018-09-02 16:52:34 -0400

    LeetCode #772

---
+++ src/BasicCalculator3.java
@@ -0,0 +1,18 @@
+// LeetCode #772 (Basic Calculator III).
+
+// Implement a basic calculator to evaluate a simple expression string.
+
+// The expression string contains only non-negative integers, +, -, *, / operators, 
+// open ( and closing parentheses ) and empty spaces. The integer division should 
+// truncate toward zero.
+
+// You may assume that the given expression is always valid. All intermediate results
+// will be in the range of [-2147483648, 2147483647].
+
+public class BasicCalculator3 {
+
+	public int calculate(String s) {
+
+	}
+
+}
