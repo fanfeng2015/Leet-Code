2c1f89b875c65335967daf35db473d9a3a50685e
Fan Feng <fanfeng2015@gmail.com>
2018-09-02 21:00:50 -0400

    LeetCode #282

--- src/ExpressionAddOperators.java
+++ src/ExpressionAddOperators.java
@@ -10,8 +10,39 @@ import java.util.List;
 public class ExpressionAddOperators {
 
 	public List<String> addOperators(String num, int target) {
+		StringBuilder sb = new StringBuilder();
 		List<String> result = new ArrayList<>();
+		DFS(num, target, sb, result, 0, 0, 0);
 		return result;
 	}
 
+	private void DFS(String num, int target, StringBuilder sb, List<String> result, int pos, long soFar, long mult) {
+		if (pos == num.length()) {
+			if (soFar == target) {
+				result.add(sb.toString());
+			}
+			return;
+		}
+		for (int i = pos; i < num.length(); i++) {
+			if (num.charAt(pos) == '0' && i != pos) {
+				break;
+			}
+			long cur = Long.parseLong(num.substring(pos, i + 1));
+			int length = sb.length();
+			if (pos == 0) {
+				DFS(num, target, sb.append(cur), result, i + 1, cur, cur);
+				sb.setLength(length);
+			} else {
+				DFS(num, target, sb.append('+').append(cur), result, i + 1, soFar + cur, cur);
+				sb.setLength(length);
+				DFS(num, target, sb.append('-').append(cur), result, i + 1, soFar - cur, -cur);
+				sb.setLength(length);
+				DFS(num, target, sb.append('*').append(cur), result, i + 1, soFar + mult * (cur - 1), mult * cur);
+				sb.setLength(length);
+			}
+		}
+	}
+
+	// Time complexity is O(3^n).
+	// Space complexity is O(n).
 }
