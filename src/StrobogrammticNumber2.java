7f502197fe8bf83ec6a867d4804a8e63d854c880
Fan Feng <fanfeng2015@gmail.com>
2018-09-04 11:06:30 -0400

    LeetCode #247

---
+++ src/StrobogrammticNumber2.java
@@ -0,0 +1,10 @@
+// LeetCode #247 (Strobogrammatic Number II).
+
+// A strobogrammatic number is a number that looks the same when rotated 180 degrees
+// (looked at upside down).
+
+// Find all strobogrammatic numbers that are of length = n.
+
+public class StrobogrammticNumber2 {
+
+}
