45455abf29d753760525e03486652c129a3558ed
Fan Feng <fanfeng2015@gmail.com>
2018-09-07 18:30:27 -0400

    LeetCode #161

---
+++ src/OneEditDistance.java
@@ -0,0 +1,44 @@
+// LeetCode #161 (One Edit Distance).
+
+// Given two strings s and t, determine if they are both one edit distance apart.
+
+// Notes: There are 3 possiblities to satisify one edit distance apart:
+
+// Insert a character into s to get t.
+// Delete a character from s to get t.
+// Replace a character of s to get t.
+
+public class OneEditDistance {
+
+	public boolean isOneEditDistance(String s, String t) {
+		int m = s.length(), n = t.length();
+		if (Math.abs(m - n) > 1) {
+			return false;
+		}
+		boolean found = false;
+		int i = 0, j = 0;
+		while (i < m && j < n) {
+			if (s.charAt(i) == t.charAt(j)) {
+				i++;
+				j++;
+			} else if (found) {
+				return false;
+			} else {
+				found = true;
+				if (m == n) {
+					i++;
+					j++;
+				} else if (m < n) {
+					j++;
+				} else {
+					i++;
+				}
+			}
+		}
+		int diff = Math.abs((m - i) - (n - j));
+		return (found && diff == 0) || (!found && diff == 1);
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(1).
+}
