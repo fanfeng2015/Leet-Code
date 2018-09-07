bf5d51ae9b80ce4d62619c746e531b0842c9ee4a
Fan Feng <fanfeng2015@gmail.com>
2018-09-07 13:47:58 -0400

    LeetCode #341

--- src/FlattenNestedListIterator.java
+++ src/FlattenNestedListIterator.java
@@ -42,19 +42,3 @@ public class FlattenNestedListIterator implements Iterator<Integer> {
 	}
 
 }
-
-interface NestedInteger {
-
-	// Returns true if this NestedInteger holds a single integer, rather than a
-	// nested list.
-	public boolean isInteger();
-
-	// Returns the single integer that this NestedInteger holds, if it holds a
-	// single integer, or null otherwise.
-	public Integer getInteger();
-
-	// Returns the nested list that this NestedInteger holds, if it holds a nested
-	// list, or null otherwise.
-	public List<NestedInteger> getList();
-
-}
