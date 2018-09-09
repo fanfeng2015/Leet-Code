bdc0ef475528011dac97ff17b9d32a2e7fc82789
Fan Feng <fanfeng2015@gmail.com>
2018-09-09 19:58:38 -0400

    LeetCode #147

---
+++ src/InsertionSortLinkedList.java
@@ -0,0 +1,25 @@
+// LeetCode #147 (Insertion Sort List).
+
+// Sort a linked list using insertion sort.
+
+public class InsertionSortLinkedList {
+
+	public ListNode insertionSortList(ListNode head) {
+		for (ListNode cur = head; cur != null; cur = cur.next) {
+			ListNode min = cur;
+			for (ListNode iter = min.next; iter != null; iter = iter.next) {
+				if (iter.val < min.val) {
+					min = iter;
+				}
+			}
+			// swap
+			int temp = cur.val;
+			cur.val = min.val;
+			min.val = temp;
+		}
+		return head;
+	}
+
+	// Time complexity is O(n^2).
+	// Space complexity is O(1).
+}
