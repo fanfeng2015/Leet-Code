bf907f657d9ebf89a2e1b9252be251a609910c28
Fan Feng <fanfeng2015@gmail.com>
2018-09-11 19:10:54 -0400

    LeetCode #291

---
+++ src/WordPattern2.java
@@ -0,0 +1,16 @@
+// LeetCode #291 (Word Pattern II).
+
+// Given a pattern and a string str, find if str follows the same pattern.
+
+// Here follow means a full match, such that there is a bijection between a letter in pattern
+// and a non-empty substring in str.
+
+// Note: You may assume both pattern and str contains only lowercase letters.
+
+public class WordPattern2 {
+
+    public boolean wordPatternMatch(String pattern, String str) {
+        
+    }
+	
+}
