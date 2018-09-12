889dd131980bb020f3cefd8093ca52a9a0472bef
Fan Feng <fanfeng2015@gmail.com>
2018-09-11 20:12:14 -0400

    LeetCode #291

--- src/WordPattern2.java
+++ src/WordPattern2.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 // LeetCode #291 (Word Pattern II).
 
 // Given a pattern and a string str, find if str follows the same pattern.
@@ -9,8 +12,35 @@
 
 public class WordPattern2 {
 
-    public boolean wordPatternMatch(String pattern, String str) {
-        
-    }
-	
+	public boolean wordPatternMatch(String pattern, String str) {
+		Map<Character, String> charToStr = new HashMap<>();
+		Map<String, Character> strToChar = new HashMap<>();
+		return DFS(pattern, str, charToStr, strToChar, 0, 0);
+	}
+
+	private boolean DFS(String pattern, String str, Map<Character, String> charToStr, Map<String, Character> strToChar,
+			int m, int n) {
+		if (m == pattern.length() || n == str.length()) {
+			return (m == pattern.length()) && (n == str.length());
+		}
+		for (int i = n; i < str.length(); i++) {
+			String substr = str.substring(n, i + 1);
+			String s = charToStr.get(pattern.charAt(m));
+			Character c = strToChar.get(substr);
+			if (s == null && c == null) {
+				charToStr.put(c, s);
+				strToChar.put(s, c);
+				if (DFS(pattern, str, charToStr, strToChar, m + 1, i + 1)) {
+					return true;
+				}
+				charToStr.remove(c);
+				strToChar.remove(s);
+			} else if (s != null && c != null && s.equals(substr) && c.equals(pattern.charAt(m))) {
+				return DFS(pattern, str, charToStr, strToChar, m + 1, i + 1);
+			}
+
+		}
+		return false;
+	}
+
 }
