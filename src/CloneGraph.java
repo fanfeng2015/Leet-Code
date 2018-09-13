e0d775e857463f17162327a15986751b5491e726
Fan Feng <fanfeng2015@gmail.com>
2018-09-13 11:16:03 -0400

    LeetCode #133

--- src/CloneGraph.java
+++ src/CloneGraph.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 // LeetCode #133 (Clone Graph).
@@ -11,60 +9,27 @@ import java.util.Queue;
 
 public class CloneGraph {
 
-	// DFS
-	public List<GraphNode> copy(List<GraphNode> graph) {
-		if (graph == null) {
-			return null;
-		}
-		HashMap<GraphNode, GraphNode> map = new HashMap<>();
-		for (GraphNode node : graph) {
-			if (!map.containsKey(node)) {
-				map.put(node, new GraphNode(node.key));
-				dfs(node, map);
-			}
-		}
-		return new ArrayList<GraphNode>(map.values());
-	}
-
-	private void dfs(GraphNode node, HashMap<GraphNode, GraphNode> map) {
-		GraphNode copy = map.get(node);
-		for (GraphNode neighbor : node.neighbors) {
-			if (!map.containsKey(neighbor)) {
-				map.put(neighbor, new GraphNode(neighbor.key));
-				dfs(neighbor, map);
-			}
-			copy.neighbors.add(map.get(neighbor));
-		}
-	}
-
-	// Time complexity is O(|V|+|E|).
-	// Space complexity is O(|V|+|E|), because of call-stack.
-
 	// BFS
-	public List<GraphNode> copy2(List<GraphNode> graph) {
-		if (graph == null) {
+	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+		if (node == null) {
 			return null;
 		}
-		Queue<GraphNode> queue = new LinkedList<>();
-		HashMap<GraphNode, GraphNode> map = new HashMap<>();
-		for (GraphNode node : graph) {
-			if (!map.containsKey(node)) {
-				queue.offer(node);
-				map.put(node, new GraphNode(node.key));
-			}
-			while (!queue.isEmpty()) {
-				GraphNode cur = queue.poll();
-				GraphNode copy = map.get(cur);
-				for (GraphNode neighbor : cur.neighbors) {
-					if (!map.containsKey(neighbor)) {
-						queue.offer(neighbor);
-						map.put(neighbor, new GraphNode(neighbor.key));
-					}
-					copy.neighbors.add(map.get(neighbor));
+		Queue<UndirectedGraphNode> queue = new LinkedList<>();
+		HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+		queue.offer(node);
+		map.put(node, new UndirectedGraphNode(node.label));
+		while (!queue.isEmpty()) {
+			UndirectedGraphNode cur = queue.poll();
+			UndirectedGraphNode copy = map.get(cur);
+			for (UndirectedGraphNode neighbor : cur.neighbors) {
+				if (!map.containsKey(neighbor)) {
+					queue.offer(neighbor);
+					map.put(neighbor, new UndirectedGraphNode(neighbor.label));
 				}
+				copy.neighbors.add(map.get(neighbor));
 			}
 		}
-		return new ArrayList<GraphNode>(map.values());
+		return map.get(node);
 	}
 
 	// Time complexity is O(|V|+|E|).
