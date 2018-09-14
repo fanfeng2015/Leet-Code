21ada41e81ab721908785db0ec632682466d8b43
Fan Feng <fanfeng2015@gmail.com>
2018-09-14 13:11:19 -0400

    LeetCode #394

--- src/DecodeString.java
+++ src/DecodeString.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 // LeetCode #394 (Decode String).
 
 // Given an encoded string, return it's decoded string.
@@ -16,9 +18,50 @@
 public class DecodeString {
 
 	public String decodeString(String s) {
+		int parentheses = 0;
+		StringBuilder result = new StringBuilder();
+		Stack<Integer> counts = new Stack<>();
+		Stack<String> sequences = new Stack<>();
+		int i = 0;
+		while (i < s.length()) {
+			if (s.charAt(i) == '[') {
+				i++;
+				parentheses++;
+			} else if (s.charAt(i) == ']') {
+				i++;
+				parentheses--;
+				int count = counts.pop();
+				String sequence = sequences.pop();
+				StringBuilder temp = new StringBuilder();
+				for (int j = 0; j < count; j++) {
+					temp.append(sequence);
+				}
 
+				if (parentheses > 0) {
+					String next = ((sequences.isEmpty()) ? "" : sequences.pop()) + temp.toString();
+					sequences.push(next);
+				} else {
+					result.append(temp);
+				}
+
+			} else if (Character.isDigit(s.charAt(i))) {
+				int count = Character.getNumericValue(s.charAt(i++));
+				while (i < s.length() && Character.isDigit(s.charAt(i))) {
+					count = 10 * count + Character.getNumericValue(s.charAt(i++));
+				}
+				counts.push(count);
+			} else {
+				int j = i;
+				while (j < s.length() && !Character.isDigit(s.charAt(j)) && s.charAt(j) != '[' && s.charAt(j) != ']') {
+					j++;
+				}
+				sequences.add(s.substring(i, j));
+				i = j;
+			}
+		}
+		return result.toString();
 	}
-	
-	// Time complexity is 
+
+	// Time complexity is
 
 }
