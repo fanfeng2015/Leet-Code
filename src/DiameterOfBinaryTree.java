22d7f246a13a5682ad4e6b73587f6af9f61ef087
Fan Feng <fanfeng2015@gmail.com>
2018-09-30 15:28:52 -0400

    LeetCode #543

--- src/DiameterOfBinaryTree.java
+++ src/DiameterOfBinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // LeetCode #543 (Diameter of Binary Tree).
 
 // Given a binary tree, you need to compute the length of the diameter of the tree. 
@@ -10,7 +13,7 @@ public class DiameterOfBinaryTree {
 
 	public int diameterOfBinaryTree(TreeNode root) {
 		helper(root);
-		return Math.max(max, 0);
+		return max;
 	}
 
 	// Returns the length of the longest path that starts from the root node.
@@ -23,7 +26,43 @@ public class DiameterOfBinaryTree {
 		max = Math.max(max, left + right);
 		return Math.max(left, right) + 1;
 	}
-	
+
 	// Time complexity is O(n).
 	// Space complexity is O(n).
+
+/*	
+	// Follow up (Facebook): Print the longest path.
+	private List<Integer> longest = new ArrayList<>();
+
+	public List<Integer> longestPathOfBinaryTree(TreeNode root) {
+		longest(root);
+		return longest;
+	}
+
+	// Returns the longest path that starts from the root node.
+	private List<Integer> longest(TreeNode root) {
+		if (root == null) {
+			return new ArrayList<>();
+		}
+		List<Integer> left = longest(root.left);
+		List<Integer> right = longest(root.right);
+		if (left.size() + right.size() > longest.size()) {
+			longest = new ArrayList<>(left);
+			longest.add(root.val);
+			longest.addAll(reverse(right));
+		}
+		List<Integer> longer = (left.size() < right.size()) ? right : left;
+		longer.add(root.val);
+		return longer;
+	}
+
+	private List<Integer> reverse(List<Integer> list) {
+		List<Integer> result = new ArrayList<>();
+		for (int i = 0; i < list.size(); i++) {
+			result.add(list.get(list.size() - 1 - i));
+		}
+		return result;
+	}
+*/
+
 }
