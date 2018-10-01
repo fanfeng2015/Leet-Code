a0c4f67041b1db3b23831309c56941cb8bd70a19
Fan Feng <fanfeng2015@gmail.com>
2018-09-30 23:54:01 -0400

    LeetCode #5

---
+++ src/LongestPalindromicSubstring.java
@@ -0,0 +1,32 @@
+// LeetCode #5 (Longest Palindromic Substring).
+
+// Given a string s, find the longest palindromic substring in s.
+
+// You may assume that the maximum length of s is 1000.
+
+public class LongestPalindromicSubstring {
+
+	private int lo, max;
+
+	public String longestPalindrome(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			extendOnBothSides(s, i, i);
+			extendOnBothSides(s, i, i + 1);
+		}
+		return s.substring(lo, lo + max);
+	}
+
+	private void extendOnBothSides(String s, int left, int right) {
+		while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+			left--;
+			right++;
+		}
+		if (max < right - left - 1) {
+			lo = left + 1;
+			max = right - left - 1;
+		}
+	}
+
+	// Time complexity is O(n^2).
+	// Space complexity is O(1).
+}
