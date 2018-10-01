dd320853ff8a04b5c05f12bb6295b540315b6e97
Fan Feng <fanfeng2015@gmail.com>
2018-10-01 11:27:12 -0400

    LeetCode #159

---
+++ src/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,33 @@
+// LeetCode #159 (Longest Substring with At Most Two Distinct Characters).
+
+// Given a string s, find the length of the longest substring t that contains 
+// at most 2 distinct characters.
+
+public class LongestSubstringWithAtMostTwoDistinctCharacters {
+
+	public int lengthOfLongestSubstringTwoDistinct(String s) {
+		int max = 0, lo = 0;
+		int count = 0;
+		int[] countMap = new int[256];
+		for (int i = 0; i < s.length(); i++) {
+			if (countMap[s.charAt(i)]++ == 0) {
+				count++;
+			}
+			if (count <= 2) {
+				max = Math.max(max, i - lo + 1);
+			}
+			while (count > 2) {
+				if (countMap[s.charAt(lo++)]-- == 1) {
+					count--;
+				}
+			}
+		}
+		return max;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(1).
+
+	// Follow up: LeetCode #340 (Longest Substring with At Most K Distinct
+	// Characters).
+}
