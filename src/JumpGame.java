07028df56be37db60c156573bb4d362972758823
Fan Feng <fanfeng2015@gmail.com>
2018-10-02 11:15:12 -0400

    LeetCode #55

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -8,9 +8,27 @@
 // Determine if you are able to reach the last index.
 
 public class JumpGame {
+	// DP
+	// M[i]: from index 0, can jump to index i
+	public boolean canJump(int[] array) {
+		boolean[] canJump = new boolean[array.length];
+		canJump[0] = true;
+		for (int i = 1; i < array.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (canJump[j] && j + array[j] >= i) {
+					canJump[i] = true;
+					break;
+				}
+			}
+		}
+		return canJump[canJump.length - 1];
+	}
+
+	// Time complexity is O(n^2).
+	// Space complexity is O(n).
 
 	// Greedy
-	public boolean canJump(int[] nums) {
+	public boolean canJump2(int[] nums) {
 		int max = 0;
 		for (int i = 0; i < nums.length; i++) {
 			max = (max < i) ? max : Math.max(max, i + nums[i]);
