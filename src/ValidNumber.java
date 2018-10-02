d9adbec4af9bd6623b8912fce73756bb0f264799
Fan Feng <fanfeng2015@gmail.com>
2018-10-02 00:32:33 -0400

    LeetCode #65

--- src/ValidNumber.java
+++ src/ValidNumber.java
@@ -6,24 +6,25 @@ public class ValidNumber {
 
 	public boolean isNumber(String s) {
 		s = s.trim();
+		// indicate existence
 		boolean number = false, numberAfterE = true, point = false, e = false;
 		for (int i = 0; i < s.length(); i++) {
 			if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
 				number = true;
 				numberAfterE = true;
-			} else if (s.charAt(i) == '.') {
+			} else if (s.charAt(i) == '.') { // . can't be proceeded by e or .
 				if (e || point) {
 					return false;
 				}
 				point = true;
-			} else if (s.charAt(i) == 'e') {
+			} else if (s.charAt(i) == 'e') { // e can't be processed by e or non-digit
 				if (e || !number) {
 					return false;
 				}
 				e = true;
 				numberAfterE = false;
 			} else if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-				// the previous char must be e
+				// either the first char, or the previous char is e
 				if (i != 0 && s.charAt(i - 1) != 'e') {
 					return false;
 				}
