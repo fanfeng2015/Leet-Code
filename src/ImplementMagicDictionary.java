2e798b8ff48546fbfa5c8f68ea545cdea3f724e4
Fan Feng <fanfeng2015@gmail.com>
2018-10-11 00:59:33 -0400

    LeetCode #676

--- src/ImplementMagicDictionary.java
+++ src/ImplementMagicDictionary.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 public class ImplementMagicDictionary {
 
+	// Solution 1
 	Map<Integer, HashSet<String>> map;
 
 	public ImplementMagicDictionary() {
@@ -53,7 +54,65 @@ public class ImplementMagicDictionary {
 
 	// n: size of dictionary, k: length of dictionary word, l: length of search word
 
-	// Time complexity is O(n*k) to build dictionary, O(n*l) to search word.
+	// Time complexity is O(n) to build dictionary, O(n*l) to search word.
 	// Space complexity is O(n*k).
+
+/*
+	// Solution 2: Trie
+	TrieNode root;
+
+	public ImplementMagicDictionary() {
+		root = new TrieNode();
+	}
+
+	public void buildDict(String[] dict) {
+		for (String word : dict) {
+			TrieNode cur = root;
+			for (int i = 0; i < word.length(); i++) {
+				if (cur.children[word.charAt(i) - 'a'] == null) {
+					cur.children[word.charAt(i) - 'a'] = new TrieNode();
+				}
+				cur = cur.children[word.charAt(i) - 'a'];
+			}
+			cur.isWord = true;
+		}
+	}
+
+	public boolean search(String word) {
+		TrieNode cur = root;
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			for (int j = 0; j < 26; j++) {
+				// modify the i-th char to every possible char
+				if ((char) ('a' + j) != ch && cur.children[j] != null && search(word, i + 1, cur.children[j])) {
+					return true;
+				}
+			}
+			// modification of the i-th char doesn't lead to an existing word in dict
+			if (cur.children[ch - 'a'] == null) {
+				return false;
+			}
+			cur = cur.children[ch - 'a'];
+		}
+		return false;
+	}
+
+	// Modification has been made at (index - 1), an exact match for the rest of the
+	// word needs to be found.
+	private boolean search(String word, int index, TrieNode node) {
+		TrieNode cur = node;
+		for (int i = index; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			if (cur.children[ch - 'a'] == null) {
+				return false;
+			}
+			cur = cur.children[ch - 'a'];
+		}
+		return cur.isWord;
+	}
+	
+	// Time complexity is O(n*k) to build dictionary, O(l) to search word.
+	// Space complexity is O(n*k).
+*/
 
 }
