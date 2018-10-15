115e9c88536be971e288fe0e56bc0a0c5650b9bd
Fan Feng <fanfeng2015@gmail.com>
2018-10-14 23:17:56 -0400

    LeetCode #489

---
+++ src/RobotRoomCleaner.java
@@ -0,0 +1,30 @@
+// LeetCode #489 (Robot Room Cleaner).
+
+// Given a robot cleaner in a room modeled as a grid.
+
+// Each cell in the grid can be empty or blocked.
+
+// The robot cleaner with 4 given APIs can move forward, turn left or turn right. 
+// Each turn it made is 90 degrees.
+
+// When it tries to move into a blocked cell, its bumper sensor detects the obstacle
+// and it stays on the current cell.
+
+// Notes:
+// 1. The input is only given to initialize the room and the robot's position internally. 
+//    You must solve this problem "blindfolded". In other words, you must control the robot
+//    using only the mentioned 4 APIs, without knowing the room layout and the initial robot's
+//    position.
+// 2. The robot's initial position will always be in an accessible cell.
+// 3. The initial direction of the robot will be facing up.
+// 4. All accessible cells are connected, which means the all cells marked as 1 will be accessible
+//    by the robot.
+// 5. Assume all four edges of the grid are all surrounded by wall.
+
+public class RobotRoomCleaner {
+
+	public void cleanRoom(Robot robot) {
+
+	}
+
+}
