87f41613444e1920fa232551fa62386ccf047d6a
Fan Feng <fanfeng2015@gmail.com>
2018-10-14 23:11:01 -0400

    LeetCode #70

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -20,4 +20,10 @@ public class ClimbingStairs {
 
 	// Time complexity is O(n).
 	// Space complexity is O(1).
+
+	// Follow up: Each time you can jump any number of steps in [1, k].
+	// k = 3 -> [1, 2, 3, 6, 11, ...]
+	// O(n*k) time, O(k) space. -> O(n) time, O(k) space.
+	// Keep an array list of [1, 2, 3], and a sum of the current list. Each time,
+	// append the sum, remove the head element, and adjust sum accordingly.
 }
