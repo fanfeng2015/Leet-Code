2e8adc1157fd73ff42118a8e5380eebd8f3044f5
Fan Feng <fanfeng2015@gmail.com>
2023-10-05 14:19:05 -0700

    LeetCode #658

--- src/FindKClosestElements.java
+++ src/FindKClosestElements.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//LeetCode #658 (Find K Closest Elements).
+// LeetCode #658 (Find K Closest Elements).
 
 // Given a sorted integer array arr, two integers k and x, return the k closest integers to x 
 // in the array. The result should also be sorted in ascending order.
