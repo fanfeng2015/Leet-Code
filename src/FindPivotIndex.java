6f8abc8336053aaeb13da40de502a4ab95818f9a
Fan Feng <fanfeng2015@gmail.com>
2023-10-05 17:59:56 -0700

    LeetCode #724

---
+++ src/FindPivotIndex.java
@@ -0,0 +1,31 @@
+// LeetCode #724 (Find Pivot Index).
+
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the 
+// index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no 
+// elements to the left. This also applies to the right edge of the array.
+
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+public class FindPivotIndex {
+
+	public int pivotIndex(int[] nums) {
+		int sum = 0, prefix = 0;
+		for (int num : nums) {
+			sum += num;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (prefix == sum - nums[i] - prefix) {
+				return i;
+			}
+			prefix += nums[i];
+		}
+		return -1;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(1).
+}
