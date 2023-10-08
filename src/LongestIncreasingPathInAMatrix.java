f45a302fe8b4ea5cd44e9db29f0da599442a1b39
Fan Feng <fanfeng2015@gmail.com>
2023-10-07 20:03:57 -0700

    LeetCode #329

--- src/LongestIncreasingPathInAMatrix.java
+++ src/LongestIncreasingPathInAMatrix.java
@@ -17,7 +17,7 @@ public class LongestIncreasingPathInAMatrix {
 		}
 		int max = 0;
 		int m = matrix.length, n = matrix[0].length;
-		int[][] memoization = new int[m][n];
+		int[][] memoization = new int[m][n]; // length of longest path starting from (i, j)
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
 				max = Math.max(max, DFS(matrix, memoization, i, j));
