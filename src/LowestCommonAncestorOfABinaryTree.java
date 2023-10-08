8552e6cfd50dfe4fb6847916ff357f2ceacde1a3
Fan Feng <fanfeng2015@gmail.com>
2023-10-07 23:01:06 -0700

    LeetCode #236

---
+++ src/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,23 @@
+// LeetCode #236 (Lowest Common Ancestor of a Binary Tree).
+
+// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+public class LowestCommonAncestorOfABinaryTree {
+
+	// Important Assumption: The given two nodes are guaranteed to be in the binary
+	// tree.
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null || root == p || root == q) {
+			return root;
+		}
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (left != null && right != null) {
+			return root;
+		}
+		return left == null ? right : left;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(n).
+}
