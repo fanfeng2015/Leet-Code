1ba9786fde10c1464f637ca0c4cd3a3dc227ee78
Fan Feng <fanfeng2015@gmail.com>
2023-10-08 20:23:29 -0700

    LeetCode #378

--- src/KthSmallestElementInASortedMatrix.java
+++ src/KthSmallestElementInASortedMatrix.java
@@ -5,7 +5,7 @@
 
 // Note that it is the kth smallest element in the sorted order, not the kth distinct element.
 
-// You must find a solution with a memory complexity better than O(n2). 
+// You must find a solution with a memory complexity better than O(n2).
 
 import java.util.PriorityQueue;
 
