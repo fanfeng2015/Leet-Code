50a50fae783963a21f84bea62ec995b6f1339dd1
Fan Feng <fanfeng2015@gmail.com>
2023-10-08 19:46:37 -0700

    LeetCode #378

--- src/KthSmallestElementInASortedMatrix.java
+++ src/KthSmallestElementInASortedMatrix.java
@@ -1,13 +1,13 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 // LeetCode #378 (Kth Smallest Element in a Sorted Matrix).
 
-// Given a n x n matrix where each of the rows and columns are sorted in ascending 
-// order, find the kth smallest element in the matrix.
+// Given an n x n matrix where each of the rows and columns is sorted in ascending order, return 
+// the kth smallest element in the matrix.
 
-// Note that it is the kth smallest element in the sorted order, not the kth distinct
-// element.
+// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+// You must find a solution with a memory complexity better than O(n2).
+
+import java.util.PriorityQueue;
 
 public class KthSmallestElementInASortedMatrix {
 
