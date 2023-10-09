9f7aaeae3b96ad9592a227d733a559c18f583eed
Fan Feng <fanfeng2015@gmail.com>
2023-10-08 18:59:15 -0700

    LeetCode #63

--- src/UniquePaths2.java
+++ src/UniquePaths2.java
@@ -1,8 +1,13 @@
 // LeetCode #63 (Unique Paths II).
 
-// Now consider if some obstacles are added to the grids. How many unique paths could there be?
+// You are given an m x n integer array grid. There is a robot initially located at the top-left corner
+// (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). 
+// The robot can only move either down or right at any point in time.
 
-// An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+// An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot 
+// include any square that is an obstacle.
+
+// Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
 
 public class UniquePaths2 {
 
@@ -22,4 +27,28 @@ public class UniquePaths2 {
 
 	// Time complexity is O(m*n).
 	// Space complexity is O(m*n), but obviously can be optimized to O(n).
+
+	public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+		int m = obstacleGrid.length, n = obstacleGrid[0].length;
+		int[] prev = new int[n], next = new int[n];
+		for (int j = 0; j < n; j++) {
+			int left = (j == 0) ? 1 - obstacleGrid[0][0] : prev[j - 1];
+			prev[j] = (obstacleGrid[0][j] == 1) ? 0 : left;
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				int left = (j == 0) ? 0 : next[j - 1];
+				next[j] = (obstacleGrid[i][j] == 1) ? 0 : prev[j] + left;
+			}
+			prev = next;
+			next = new int[n];
+		}
+		return prev[n - 1];
+	}
+
+	// Time complexity is O(m*n).
+	// Space complexity is O(n).
+	
+	// Technically, unless the previously allocated array is deallocated, the space
+	// isn't actually saved.
 }
