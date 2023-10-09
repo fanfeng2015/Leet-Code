d896fe64c34a8f545f3c4cb9e2cfa41d295d1dff
Fan Feng <fanfeng2015@gmail.com>
2023-10-08 19:07:24 -0700

    LeetCode #463

--- src/IslandPerimeter.java
+++ src/IslandPerimeter.java
@@ -1,12 +1,14 @@
 // LeetCode #463 (Island Perimeter).
 
-// You are given a map in form of a two-dimensional integer grid where 1 represents land and 
-// 0 represents water. Grid cells are connected horizontally / vertically (not diagonally).
+// You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 
+// represents water.
 
-// The grid is completely surrounded by water, and there is exactly one island (i.e., one or 
-// more connected land cells). The island doesn't have "lakes" (water inside that isn't connected
-// to the water around the island). One cell is a square with side length 1. The grid is rectangular, 
-// width and height don't exceed 100. Determine the perimeter of the island.
+// Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by 
+// water, and there is exactly one island (i.e., one or more connected land cells).
+
+// The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island.
+// One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. 
+// Determine the perimeter of the island.
 
 public class IslandPerimeter {
 
@@ -29,7 +31,7 @@ public class IslandPerimeter {
 	// Space complexity is O(1).
 
 	// Follow up: There are multiple islands, return the longest perimeter?
-	
+
 	// DFS. Need to create a copy of grid to keep track of visited cells. The
 	// original grid will be used to determine how much the current perimeter should
 	// be incremented.
