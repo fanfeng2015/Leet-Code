9196c8c4967644f903d61f010bb631dedf6f650a
Fan Feng <fanfeng2015@gmail.com>
2023-10-14 17:34:27 -0700

    LeetCode #111

--- src/MinimumDepthOfBinaryTree.java
+++ src/MinimumDepthOfBinaryTree.java
@@ -4,8 +4,8 @@ import java.util.LinkedList;
 
 // Given a binary tree, find its minimum depth.
 
-// The minimum depth is the number of nodes along the shortest path 
-// from the root node down to the nearest leaf node.
+// The minimum depth is the number of nodes along the shortest path from the root node down to the 
+// nearest leaf node.
 
 public class MinimumDepthOfBinaryTree {
 
