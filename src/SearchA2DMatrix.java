f0d502fe0df3d3209b59d8bb9642702a84fb9983
Fan Feng <fanfeng2015@gmail.com>
2023-10-15 15:47:51 -0700

    LeetCode #74

--- src/SearchA2DMatrix.java
+++ src/SearchA2DMatrix.java
@@ -1,8 +1,8 @@
 // LeetCode #74 (Search a 2D Matrix).
 
 // You are given an m x n integer matrix matrix with the following two properties:
-//   -- Each row is sorted in non-decreasing order.
-//   -- The first integer of each row is greater than the last integer of the previous row.
+// 1. Each row is sorted in non-decreasing order.
+// 2. The first integer of each row is greater than the last integer of the previous row.
 
 // Given an integer target, return true if target is in matrix or false otherwise.
 
