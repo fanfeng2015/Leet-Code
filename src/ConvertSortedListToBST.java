4c0acf944c071e623321c1eca4e028cdb34b7ae3
Fan Feng <fanfeng2015@gmail.com>
2023-10-17 16:15:12 -0700

    LeetCode #109

--- src/ConvertSortedListToBST.java
+++ src/ConvertSortedListToBST.java
@@ -1,10 +1,7 @@
 // LeetCode #109 (Convert Sorted List to Binary Search Tree).
 
-// Given a singly linked list where elements are sorted in ascending order, convert it to 
-// a height balanced BST.
-
-// For this problem, a height-balanced binary tree is defined as a binary tree in which the
-// depth of the two subtrees of every node never differ by more than 1.
+// Given the head of a singly linked list where elements are sorted in ascending order, convert it to a 
+// height-balanced binary search tree.
 
 public class ConvertSortedListToBST {
 
