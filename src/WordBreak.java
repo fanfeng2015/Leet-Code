335a86be323581bf50ad9fe49fa3af62965fe07a
Fan Feng <fanfeng2015@gmail.com>
2023-10-16 18:26:20 -0700

    LeetCode #139

--- src/WordBreak.java
+++ src/WordBreak.java
@@ -1,41 +1,36 @@
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 // LeetCode #139 (Word Break).
 
-// Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
-// determine if s can be segmented into a space-separated sequence of one or more dictionary
-// words.
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a 
+// space-separated sequence of one or more dictionary words.
 
-// Notes:
-// 1. The same word in the dictionary may be reused multiple times in the segmentation.
-// 2. You may assume the dictionary does not contain duplicate words.
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
 
 public class WordBreak {
 
 	// M[i]: Whether [0, i - 1] of input can be composed by concatenating words from
 	// the dictionary.
-	public boolean canBreak(String input, String[] dict) {
-		Set<String> set = new HashSet<>();
-		for (String s : dict) {
-			set.add(s);
-		}
-		boolean[] array = new boolean[input.length() + 1];
-		array[0] = true;
-		for (int i = 1; i <= input.length(); i++) {
+	public boolean wordBreak(String s, List<String> wordDict) {
+		Set<String> dict = new HashSet<>(wordDict);
+		boolean[] dp = new boolean[s.length() + 1];
+		dp[0] = true;
+		for (int i = 1; i <= s.length(); i++) {
 			for (int j = 0; j < i; j++) {
-				if (array[j] && set.contains(input.substring(j, i))) {
-					array[i] = true;
+				if (dp[j] && dict.contains(s.substring(j, i))) {
+					dp[i] = true;
 					break;
 				}
 			}
 		}
-		return array[input.length()];
+		return dp[s.length()];
 	}
 
-	// Let m be the length of String s, and n be the size of the dictionary.
-	// Time complexity is O(n + m^3).
-	// Space complexity is O(n + m).
+	// Let s be the length of String s, and n be the size of the dictionary.
+	// Time complexity is O(s + n^3).
+	// Space complexity is O(s + n).
 
 	// Follow up: Return all possible compositions?
 	// LeetCode #140 (Word Break II).
