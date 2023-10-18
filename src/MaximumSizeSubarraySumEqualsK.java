8cd901c8126303bee115307c6a3444b474d18821
Fan Feng <fanfeng2015@gmail.com>
2023-10-17 23:19:16 -0700

    LeetCode #325

--- src/MaximumSizeSubarraySumEqualsK.java
+++ src/MaximumSizeSubarraySumEqualsK.java
@@ -3,26 +3,22 @@ import java.util.Map;
 
 // LeetCode #325 (Maximum Size Subarray Sum Equals k).
 
-// Given an array nums and a target value k, find the maximum length of a subarray that 
-// sums to k. If there isn't one, return 0 instead.
-
-// Note: The sum of the entire nums array is guaranteed to fit within the 32-bit signed
-// integer range.
+// Given an integer array nums and an integer k, return the maximum length of a subarray that sums to k. 
+// If there is not one, return 0 instead.
 
 public class MaximumSizeSubarraySumEqualsK {
 
 	public int maxSubArrayLen(int[] nums, int k) {
-		int max = 0;
-		int prefixSum = 0;
-		Map<Integer, Integer> map = new HashMap<>(); // { prefix: index }
+		int sum = 0, max = 0;
+		Map<Integer, Integer> map = new HashMap<>();
 		map.put(0, -1);
 		for (int i = 0; i < nums.length; i++) {
-			prefixSum += nums[i];
-			if (map.containsKey(prefixSum - k)) {
-				max = Math.max(max, i - map.get(prefixSum - k));
+			sum += nums[i];
+			if (map.containsKey(sum - k)) {
+				max = Math.max(max, i - map.get(sum - k));
 			}
-			if (!map.containsKey(prefixSum)) {
-				map.put(prefixSum, i);
+			if (!map.containsKey(sum)) {
+				map.put(sum, i); // if sum is already in the map, do nothing
 			}
 		}
 		return max;
