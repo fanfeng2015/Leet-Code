1bb7fcbfe8277beb06cb9237b19bbf764260b0ae
Fan Feng <fanfeng2015@gmail.com>
2023-10-17 23:33:58 -0700

    LeetCode #325

--- src/MaximumSizeSubarraySumEqualsK.java
+++ src/MaximumSizeSubarraySumEqualsK.java
@@ -10,7 +10,7 @@ public class MaximumSizeSubarraySumEqualsK {
 
 	public int maxSubArrayLen(int[] nums, int k) {
 		int sum = 0, max = 0;
-		Map<Integer, Integer> map = new HashMap<>();
+		Map<Integer, Integer> map = new HashMap<>(); // { sum: earliest index }
 		map.put(0, -1);
 		for (int i = 0; i < nums.length; i++) {
 			sum += nums[i];
