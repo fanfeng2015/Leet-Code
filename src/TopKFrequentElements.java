13466f7766d361b069663e13848314bf4888dbb5
Fan Feng <fanfeng2015@gmail.com>
2023-10-19 16:28:32 -0700

    LeetCode #347

---
+++ src/TopKFrequentElements.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+// LeetCode #347 (Top K Frequent Elements).
+
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the 
+// answer in any order.
+
+public class TopKFrequentElements {
+
+	public int[] topKFrequent(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			Integer count = map.get(nums[i]);
+			count = (count == null) ? 1 : count + 1;
+			map.put(nums[i], count);
+		}
+		PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(k,
+				(e1, e2) -> (e1.getValue() - e2.getValue()));
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			minHeap.offer(entry);
+			if (minHeap.size() > k) {
+				minHeap.poll();
+			}
+		}
+		int[] result = new int[k];
+		for (int i = 0; i < k; i++) {
+			result[i] = minHeap.poll().getKey();
+		}
+		return result;
+	}
+
+	// Time complexity is O(n + n*log(k) + k*log(k)) = O(n*log(k)).
+	// Space complexity is O(n + k);
+}
