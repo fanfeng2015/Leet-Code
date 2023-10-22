a9470801e37389e9dc60bb27c8dc2e08ee0b8200
Fan Feng <fanfeng2015@gmail.com>
2023-10-21 18:56:33 -0700

    LeetCode #200

--- src/NumberOfIslands.java
+++ src/NumberOfIslands.java
@@ -12,9 +12,7 @@ public class NumberOfIslands {
 		if (grid == null || grid.length == 0) {
 			return 0;
 		}
-		int row = grid.length;
-		int col = grid[0].length;
-		int count = 0;
+		int row = grid.length, col = grid[0].length, count = 0;
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
 				if (grid[i][j] == '1') {
