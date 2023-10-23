f020eb52e60493853f4ce93722281b8d52e7744f
Fan Feng <fanfeng2015@gmail.com>
2023-10-23 15:21:52 -0700

    LeetCode #169

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,9 +1,8 @@
 // LeetCode #169 (Majority Element).
 
-// Given an array of size n, find the majority element. The majority element is
-// the element that appears more than ⌊ n/2 ⌋ times.
+// Given an array nums of size n, return the majority element.
 
-// You may assume that the array is non-empty and the majority element always 
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always
 // exists in the array.
 
 public class MajorityElement {
