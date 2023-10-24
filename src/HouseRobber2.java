251ea42b4810d86aa35a78ba3a44a86a5a21edac
Fan Feng <fanfeng2015@gmail.com>
2023-10-23 22:51:54 -0700

    LeetCode #213

--- src/HouseRobber2.java
+++ src/HouseRobber2.java
@@ -1,14 +1,12 @@
 // LeetCode #213 (House Robber II).
 
-// After robbing those houses on that street, the thief has found himself a new place for his 
-// thievery so that he will not get too much attention. This time, all houses at this place are
-// arranged in a circle. 
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money 
+// stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the
+// last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the
+// police if two adjacent houses were broken into on the same night.
 
-// That means the first house is the neighbor of the last one. Meanwhile, the security system 
-// for these houses remain the same as for those in the previous street.
-
-// Given a list of non-negative integers representing the amount of money of each house, determine
-// the maximum amount of money you can rob tonight without alerting the police.
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money
+// you can rob tonight without alerting the police.
 
 public class HouseRobber2 {
 
