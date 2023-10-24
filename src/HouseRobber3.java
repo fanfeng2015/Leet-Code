429882a8ed93b5914ac0869f0cff61d15f7b5c99
Fan Feng <fanfeng2015@gmail.com>
2023-10-23 23:04:50 -0700

    LeetCode #337

--- src/HouseRobber3.java
+++ src/HouseRobber3.java
@@ -1,13 +1,12 @@
 // LeetCode #337 (House Robber III).
 
-// The thief has found himself a new place for his thievery again. There is only one entrance
-// to this area, called the "root." 
+// The thief has found himself a new place for his thievery again. There is only one entrance to this area, called 
+// root.
 
-// Besides the root, each house has one and only one parent house. After a tour, the smart thief
-// realized that "all houses in this place forms a binary tree". It will automatically contact 
-// the police if two directly-linked houses were broken into on the same night.
+// Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all
+// houses in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
 
-// Determine the maximum amount of money the thief can rob tonight without alerting the police.
+// Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
 
 public class HouseRobber3 {
 
