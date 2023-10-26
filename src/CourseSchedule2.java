edec1e473a751291a2376fb72af1130eeff44b11
Fan Feng <fanfeng2015@gmail.com>
2023-10-26 15:49:16 -0700

    LeetCode #210

--- src/CourseSchedule2.java
+++ src/CourseSchedule2.java
@@ -4,16 +4,14 @@ import java.util.List;
 
 // LeetCode #210 (Course Schedule II).
 
-// There are a total of n courses you have to take, labeled from 0 to n-1.
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array 
+// prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take 
+// course ai.
 
-// Some courses may have prerequisites, for example to take course 0 you have
-// to first take course 1, which is expressed as a pair [0, 1].
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 
-// Given the total number of courses and a list of prerequisite pairs, return 
-// the ordering of courses you should take to finish all courses.
-
-// There may be multiple correct orders, you just need to return one of them.
-// If it is impossible to finish all courses,return an empty array.
+// Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of
+// them. If it is impossible to finish all courses, return an empty array.
 
 public class CourseSchedule2 {
 
