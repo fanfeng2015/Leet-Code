9d26cb69186cd6c4ec6f789ba50f60eb240052cd
Fan Feng <fanfeng2015@gmail.com>
2023-10-27 13:42:34 -0700

    LeetCode #216

--- src/CombinationSum3.java
+++ src/CombinationSum3.java
@@ -3,12 +3,13 @@ import java.util.List;
 
 // LeetCode #216 (Combination Sum III).
 
-// Find all possible combinations of k numbers that add up to a number n, given that only numbers
-// from 1 to 9 can be used and each combination should be a unique set of numbers.
+// Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
 
-// Assumptions:
-// 1. All numbers will be positive integers.
-// 2. The solution set must not contain duplicate combinations.
+// - Only numbers 1 through 9 are used.
+// - Each number is used at most once.
+
+// Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be
+// returned in any order.
 
 public class CombinationSum3 {
 
