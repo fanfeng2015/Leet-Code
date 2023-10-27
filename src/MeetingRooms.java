b615195c16225bf2b5608a83e342e92f5fb9c069
Fan Feng <fanfeng2015@gmail.com>
2023-10-26 23:41:30 -0700

    LeetCode #252

--- src/MeetingRooms.java
+++ src/MeetingRooms.java
@@ -2,21 +2,9 @@ import java.util.Arrays;
 
 // LeetCode #252 (Meeting Rooms).
 
-// Given an array of meeting time intervals consisting of start and end times 
-// [[s1, e1], [s2, e2], ...] (si < ei), determine if a person could attend all
-// meetings.
+// Given an array of meeting time intervals where intervals[i] = [start_i, end_i], determine if a person could attend all meetings.
 
 public class MeetingRooms {
-
-	public boolean canAttendMeetings(Interval[] intervals) {
-		Arrays.sort(intervals, (a, b) -> (a.start - b.start));
-		for (int i = 0; i < intervals.length - 1; i++) {
-			if (intervals[i].end > intervals[i + 1].start) {
-				return false;
-			}
-		}
-		return true;
-	}
 
 	public boolean canAttendMeetings(int[][] intervals) {
 		Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
