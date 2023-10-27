4ad8ec18a742123355ba1d040daa8a637fcd2cb2
Fan Feng <fanfeng2015@gmail.com>
2023-10-27 12:27:17 -0700

    LeetCode #322

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -2,28 +2,31 @@ import java.util.Arrays;
 
 // LeetCode #322 (Coin Change).
 
-// You are given coins of different denominations and a total amount of money amount. 
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total 
+// amount of money.
 
-// Write a function to compute the fewest number of coins that you need to make up that amount. 
-// If that amount of money cannot be made up by any combination of the coins, return -1.
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination
+// of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
 
 public class CoinChange {
 
-	// M[i]: fewest number of coins that are needed to make up amount i
+	// dp[i]: fewest number of coins that are needed to make up amount i
 	public int coinChange(int[] coins, int amount) {
-		int[] M = new int[amount + 1];
-		Arrays.fill(M, amount + 1);
-		M[0] = 0;
+		int[] dp = new int[amount + 1];
+		Arrays.fill(dp, amount + 1);
+		dp[0] = 0;
 		for (int i = 1; i <= amount; i++) {
 			for (int j = 0; j < coins.length; j++) {
 				if (i - coins[j] >= 0) {
-					M[i] = Math.min(M[i], M[i - coins[j]] + 1);
+					dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
 				}
 			}
 		}
-		return (M[amount] == amount + 1) ? -1 : M[amount];
+		return dp[amount] == (amount + 1) ? -1 : dp[amount];
 	}
 
-	// Time complexity is O(n*amount).
+	// Time complexity is O(amount * n), where n is the number of coins.
 	// Space complexity is O(amount).
 }
