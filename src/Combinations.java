c89077710b113b9cc143de5913c0682c2c5bc92e
Fan Feng <fanfeng2015@gmail.com>
2023-10-27 13:13:25 -0700

    LeetCode #77

--- src/Combinations.java
+++ src/Combinations.java
@@ -3,13 +3,16 @@ import java.util.List;
 
 // LeetCode #77 (Combinations).
 
-// Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+// Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
+// You may return the answer in any order.
 
 public class Combinations {
 
+	// ------------------------------ 2018 ------------------------------
 	public List<List<Integer>> combine(int n, int k) {
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		List<Integer> cur = new ArrayList<>();
+		List<List<Integer>> result = new ArrayList<>();
 		DFS(n, k, cur, result);
 		return result;
 	}
@@ -27,6 +30,31 @@ public class Combinations {
 			DFS(n, k, cur, result);
 			cur.remove(cur.size() - 1);
 		}
+	}
+
+	// Time complexity is O(n^k).
+	// Space complexity is O(k).
+
+	// ------------------------------ 2023 ------------------------------
+	public List<List<Integer>> combine2(int n, int k) {
+		List<Integer> cur = new ArrayList<>();
+		List<List<Integer>> result = new ArrayList<>();
+		dfs(n, k, 1, cur, result);
+		return result;
+	}
+
+	private void dfs(int n, int k, int num, List<Integer> cur, List<List<Integer>> result) {
+		if (k == 0) {
+			result.add(new ArrayList<Integer>(cur));
+			return;
+		}
+		if (num > n) {
+			return;
+		}
+		cur.add(num);
+		dfs(n, k - 1, num + 1, cur, result);
+		cur.remove(cur.size() - 1);
+		dfs(n, k, num + 1, cur, result);
 	}
 
 	// Time complexity is O(n^k).
