56915be8a9dd297aa0b7db0ca8063ffd1d515516
Fan Feng <fanfeng2015@gmail.com>
2023-10-26 23:41:42 -0700

    LeetCode #252

--- src/MeetingRooms.java
+++ src/MeetingRooms.java
@@ -6,6 +6,16 @@ import java.util.Arrays;
 
 public class MeetingRooms {
 
+	public boolean canAttendMeetings(Interval[] intervals) {
+		Arrays.sort(intervals, (a, b) -> (a.start - b.start));
+		for (int i = 0; i < intervals.length - 1; i++) {
+			if (intervals[i].end > intervals[i + 1].start) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public boolean canAttendMeetings(int[][] intervals) {
 		Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
 		for (int i = 0; i < intervals.length - 1; i++) {
