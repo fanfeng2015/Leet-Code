9a28bacf842d2e266b2012a2b847b660d58a1b8b
Fan Feng <fanfeng2015@gmail.com>
2023-11-01 23:49:49 -0700

    LeetCode #731

--- src/MyCalendar2.java
+++ src/MyCalendar2.java
@@ -5,32 +5,24 @@ import java.util.TreeMap;
 
 // LeetCode #731 (My Calendar II).
 
-// Implement a MyCalendar2 class to store your events. A new event can be added if adding the event will not
-// cause a triple booking.
+// You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a triple 
+// booking.
 
-// Your class will have one method, book(int start, int end). Formally, this represents a booking on the half
-// open interval [start, end), the range of real numbers x such that start <= x < end.
+// A triple booking happens when three events have some non-empty intersection (i.e., some moment is common to all the three 
+// events.).
 
-// A triple booking happens when three events have some non-empty intersection (ie., there is some time that 
-// is common to all 3 events.)
+// The event can be represented as a pair of integers start and end that represents a booking on the half-open interval 
+// [start, end), the range of real numbers x such that start <= x < end.
 
-// For each call to the method MyCalendar.book, return true if the event can be added to the calendar successfully
-// without causing a triple booking. Otherwise, return false and do not add the event to the calendar.
+// Implement the MyCalendarTwo class:
+
+// - MyCalendarTwo() Initializes the calendar object.
+// - boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a 
+//   triple booking. Otherwise, return false and do not add the event to the calendar.
 
 public class MyCalendar2 {
 
 	public MyCalendar2() {
-
-	}
-
-	private static class Interval {
-		private int start;
-		private int end;
-
-		public Interval(int start, int end) {
-			this.start = start;
-			this.end = end;
-		}
 	}
 
 	// Solution 1: Brute force
@@ -55,6 +47,16 @@ public class MyCalendar2 {
 	// Time complexity is O(n).
 	// Space complexity is O(n).
 
+	private static class Interval {
+		private int start;
+		private int end;
+
+		public Interval(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+	}
+
 	// Solution 2: Red-black tree (TreeMap in Java).
 	// { t: count of intervals that start from t (+1) or end at t (-1) }
 	TreeMap<Integer, Integer> calendar = new TreeMap<>();
@@ -65,7 +67,7 @@ public class MyCalendar2 {
 		int overlap = 0;
 		for (Map.Entry<Integer, Integer> entry : calendar.entrySet()) { // in ascending key order
 			overlap += entry.getValue();
-			if (overlap == 3) { // number of start - number of end = 3
+			if (overlap == 3) { // can't book, need to remove
 				calendar.put(start, calendar.get(start) - 1);
 				calendar.put(end, calendar.get(end) + 1);
 				return false;
@@ -74,6 +76,7 @@ public class MyCalendar2 {
 		return true;
 	}
 
-	// Time complexity is O(n).
+	// Time complexity is O(n)
+	// Note that put() method takes O(log) time, but is only one operation.
 	// Space complexity is O(n).
 }
