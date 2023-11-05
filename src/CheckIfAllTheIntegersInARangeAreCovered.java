5ef94c638e54bff5c3dc3f952fee7a462885e193
Fan Feng <fanfeng2015@gmail.com>
2023-11-04 17:06:08 -0700

    LeetCode #1893

--- src/CheckIfAllTheIntegersInARangeAreCovered.java
+++ src/CheckIfAllTheIntegersInARangeAreCovered.java
@@ -1,7 +1,11 @@
 // LeetCode #1893 (Check if All the Integers in a Range Are Covered). 
 
-// You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of 
-// the ith person.
+// You are given a 2D integer array ranges and two integers left and right. Each ranges[i] = [starti, endi] represents an inclusive interval 
+// between starti and endi.
+
+// Return true if each integer in the inclusive range [left, right] is covered by at least one interval in ranges. Return false otherwise.
+
+// An integer x is covered by an interval ranges[i] = [starti, endi] if starti <= x <= endi.
 
 public class CheckIfAllTheIntegersInARangeAreCovered {
 
@@ -21,4 +25,6 @@ public class CheckIfAllTheIntegersInARangeAreCovered {
 		return true;
 	}
 
+	// Time complexity is O(m + n).
+	// Space complexity is O(m + n).
 }
