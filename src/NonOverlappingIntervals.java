c5813ecbcc7f6bee3e4bef210fa4d63356738b90
Fan Feng <fanfeng2015@gmail.com>
2023-11-04 18:33:24 -0700

    LeetCode #435

--- src/NonOverlappingIntervals.java
+++ src/NonOverlappingIntervals.java
@@ -2,23 +2,23 @@ import java.util.Arrays;
 
 // LeetCode #435 (Non-overlapping Intervals).
 
-// Given a binary array nums, return the maximum length of a contiguous subarray with an equal number
-// of 0 and 1.
+// Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make
+// the rest of the intervals non-overlapping.
 
 public class NonOverlappingIntervals {
 
+	// 1. Sort the intervals by start time.
+	// 2. When overlapping, count + 1, and keep the interval with smaller end time.
 	public int eraseOverlapIntervals(int[][] intervals) {
 		Arrays.sort(intervals, (i1, i2) -> (i1[0] - i2[0]));
-		int start = intervals[0][0], end = intervals[0][1], count = 0;
+		int end = intervals[0][1], count = 0; // don't need start
 		for (int i = 1; i < intervals.length; i++) {
 			int[] next = intervals[i];
-			if (end <= next[0]) {
-				start = next[0];
+			if (end <= next[0]) { // non-overlapping
 				end = next[1];
 			} else {
 				count++;
-				start = (end <= next[1]) ? start : next[0]; // keep the interval with shorter end time
-				end = (end <= next[1]) ? end : next[1];
+				end = Math.min(end, next[1]); // keep the interval with smaller end time
 			}
 		}
 		return count;
