834c99f9799642e548f8e17fcf97270294f31156
Fan Feng <fanfeng2015@gmail.com>
2023-11-04 18:25:22 -0700

    LeetCode #452

--- src/MinimumNumberOfArrowsToBurstBalloons.java
+++ src/MinimumNumberOfArrowsToBurstBalloons.java
@@ -2,28 +2,22 @@ import java.util.Arrays;
 
 // LeetCode #452 (Minimum Number of Arrows to Burst Balloons).
 
-// There are a number of spherical balloons spread in two-dimensional space. For each balloon, 
-// provided input is the start and end coordinates of the horizontal diameter. Since it's 
-// horizontal, y-coordinates don't matter and hence the x-coordinates of start and end of the
-// diameter suffice. Start is always smaller than end. There will be at most 104 balloons.
+// There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer array
+// points where points[i] = [xstart, xend] denotes a balloon whose horizontal diameter stretches between xstart and xend. You do not know the
+// exact y-coordinates of the balloons.
 
-// An arrow can be shot up exactly vertically from different points along the x-axis. A balloon
-// with xstart and xend bursts by an arrow shot at x if xstart <= x <= xend. There is no limit to
-// the number of arrows that can be shot. An arrow once shot keeps traveling up infinitely. 
+// Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis. A balloon with xstart and
+// xend is burst by an arrow shot at x if xstart <= x <= xend. There is no limit to the number of arrows that can be shot. A shot arrow keeps
+// traveling up infinitely, bursting any balloons in its path.
 
-// The problem is to find the minimum number of arrows that must be shot to burst all balloons.
+// Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
 
 public class MinimumNumberOfArrowsToBurstBalloons {
 
 	public int findMinArrowShots(int[][] points) {
-		int n = points.length;
-		if (n == 0) {
-			return 0;
-		}
-		Arrays.sort(points, (a, b) -> (a[0] - b[0]));
-		int count = 0;
-		int end = Integer.MAX_VALUE; // minimum end of all balloons that can be bursted by the current arrow
-		for (int i = 0; i < n; i++) {
+		Arrays.sort(points, (a, b) -> (a[0] < b[0]) ? -1 : 1);
+		int count = 0, end = Integer.MAX_VALUE; // minimum end of all balloons that can be bursted by the current arrow
+		for (int i = 0; i < points.length; i++) {
 			end = Math.min(end, points[i][1]);
 			if (points[i][0] > end) {
 				count++;
@@ -35,5 +29,4 @@ public class MinimumNumberOfArrowsToBurstBalloons {
 
 	// Time complexity is O(n*log(n)).
 	// Space complexity is O()
-
 }
