67c427c789e2470d9f8aa812a8cc8b122ce2a1dc
Fan Feng <fanfeng2015@gmail.com>
2023-11-12 15:56:49 -0800

    LeetCode #364

--- src/NestedListWeightSum2.java
+++ src/NestedListWeightSum2.java
@@ -3,72 +3,68 @@ import java.util.List;
 
 // LeetCode #364 (Nested List Weight Sum II).
 
-// Given a nested list of integers, return the sum of all integers in the list weighted by
-// their depth.
+// You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be
+// integers or other lists.
 
-// Each element is either an integer, or a list -- whose elements may also be integers or 
-// other lists.
+// The depth of an integer is the number of lists that it is inside of. For example, the nested list [1,[2,2],[[3],2],1] has each
+// integer's value set to its depth. Let maxDepth be the maximum depth of any integer.
 
-// Different from the previous question where weight is increasing from root to leaf, now
-// the weight is defined from bottom up. i.e., the leaf level integers have weight 1, and
-// the root level integers have the largest weight.
+// The weight of an integer is maxDepth - (the depth of the integer) + 1.
+
+// Return the sum of each integer in nestedList multiplied by its weight.
 
 public class NestedListWeightSum2 {
 
 	// Solution 1: Get depth, then sum in reverse order.
 	public int depthSumInverse(List<NestedInteger> nestedList) {
-		int depth = findDepth(nestedList, new Integer[] { 1 }, new Integer[] { 1 });
-		return depthSumInverse(nestedList, new Integer[] { 0 }, new Integer[] { depth });
+		int depth = findDepth(nestedList, 1, 1);
+		return depthSumInverse(nestedList, depth);
 	}
 
-	public int findDepth(List<NestedInteger> nestedList, Integer[] max, Integer[] depth) {
+	public int findDepth(List<NestedInteger> nestedList, int max, int depth) {
 		for (NestedInteger ni : nestedList) {
-			max[0] = Math.max(max[0], depth[0]);
+			max = Math.max(max, depth);
 			if (!ni.isInteger()) {
-				findDepth(ni.getList(), max, new Integer[] { depth[0] + 1 });
+				max = Math.max(max, findDepth(ni.getList(), max, depth + 1));
 			}
 		}
-		return max[0];
+		return max;
 	}
 
-	public int depthSumInverse(List<NestedInteger> nestedList, Integer[] sum, Integer[] depth) {
+	public int depthSumInverse(List<NestedInteger> nestedList, int depth) {
+		int sum = 0;
 		for (NestedInteger ni : nestedList) {
 			if (ni.isInteger()) {
-				sum[0] += depth[0] * ni.getInteger();
+				sum += ni.getInteger() * depth;
 			} else {
-				depthSumInverse(ni.getList(), sum, new Integer[] { depth[0] - 1 });
+				sum += depthSumInverse(ni.getList(), depth - 1);
 			}
 		}
-		return sum[0];
+		return sum;
 	}
 
-	// Time complexity is O(n).
-	// Space complexity is O(n), because of call-stack.
+	// Time complexity is O(n), where n is the number of nested integers.
+	// Space complexity is O(d), where d is the largest depth.
 
 	// Solution 2: BFS
 	public int depthSumInverse2(List<NestedInteger> nestedList) {
-		int result = 0, cur = 0;
-		LinkedList<NestedInteger> queue = new LinkedList<>();
-		for (NestedInteger ni : nestedList) {
-			queue.offerFirst(ni);
-		}
+		int sum = 0, cur = 0;
+		LinkedList<NestedInteger> queue = new LinkedList<>(nestedList);
 		while (!queue.isEmpty()) {
 			int size = queue.size();
 			for (int i = 0; i < size; i++) {
-				NestedInteger ni = queue.pollLast();
+				NestedInteger ni = queue.removeFirst();
 				if (ni.isInteger()) {
 					cur += ni.getInteger();
-				} else if (ni.getList() != null) {
-					for (NestedInteger element : ni.getList()) {
-						queue.offerFirst(element);
-					}
+				} else {
+					queue.addAll(ni.getList());
 				}
 			}
-			result += cur;
+			sum += cur;
 		}
-		return result;
+		return sum;
 	}
 
-	// Time complexity is O(n).
+	// Time complexity is O(n), where n is the number of nested integers.
 	// Space complexity is O(n).
 }
