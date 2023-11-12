4fa2983379e4d6498e586798092ce89d3a4a0b1f
Fan Feng <fanfeng2015@gmail.com>
2023-11-11 17:15:19 -0800

    LeetCode #72

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -1,12 +1,11 @@
 // LeetCode #72 (Edit Distance).
 
-// Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+// Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 
-// You have the following 3 operations permitted on a word:
-
-// 1. Insert a character
-// 2. Delete a character
-// 3. Replace a character
+// You have the following three operations permitted on a word:
+// - Insert a character
+// - Delete a character
+// - Replace a character
 
 public class EditDistance {
 
