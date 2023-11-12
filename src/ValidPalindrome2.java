bfbf84c730a82c30dba7a9677c629e8af518f601
Fan Feng <fanfeng2015@gmail.com>
2023-11-11 16:30:08 -0800

    LeetCode #680

--- src/ValidPalindrome2.java
+++ src/ValidPalindrome2.java
@@ -1,12 +1,6 @@
 // LeetCode #680 (Valid Palindrome II).
 
-// Given a non-empty string s, you may delete at most one character. 
-
-// Judge whether you can make it a palindrome.
-
-// Note: 
-// 1. The string will only contain lowercase characters a-z. 
-// 2. The maximum length of the string is 50,000.
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
 
 public class ValidPalindrome2 {
 
