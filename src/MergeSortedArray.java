8ab029f99e6bb59e7226398a1e6d5043bdec67b1
Fan Feng <fanfeng2015@gmail.com>
2023-11-13 22:30:31 -0800

    LeetCode #88

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,9 +1,13 @@
 // LeetCode #88 (Merge Sorted Array).
 
-// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
-// You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold 
-// additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n
-// respectively.
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of
+// elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 
+// has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and 
+// should be ignored. nums2 has a length of n.
 
 public class MergeSortedArray {
 
