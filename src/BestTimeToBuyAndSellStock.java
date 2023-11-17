be71502b86b525571546ff9f217143ae02fc4803
Fan Feng <fanfeng2015@gmail.com>
2023-11-17 00:58:07 -0800

    LeetCode #121

--- src/BestTimeToBuyAndSellStock.java
+++ src/BestTimeToBuyAndSellStock.java
@@ -1,15 +1,27 @@
 // LeetCode #121 (Best Time to Buy and Sell Stock).
 
-// Say you have an array for which the i-th element is the price of a given stock on day i.
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-// If you were only permitted to complete at most one transaction (i.e., buy one and sell
-// one share of the stock), design an algorithm to find the maximum profit.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to 
+// sell that stock.
 
-// Note that you cannot sell a stock before you buy one.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 public class BestTimeToBuyAndSellStock {
 
 	public int maxProfit(int[] prices) {
+		int minPrice = prices[0], maxProfit = 0;
+		for (int i = 0; i < prices.length; i++) {
+			minPrice = Math.min(minPrice, prices[i]);
+			maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+		}
+		return maxProfit;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(1).
+
+	public int maxProfit2(int[] prices) {
 		int max = 0, maxCur = 0;
 		for (int i = 1; i < prices.length; i++) {
 			maxCur = Math.max(0, maxCur + prices[i] - prices[i - 1]);
@@ -21,7 +33,7 @@ public class BestTimeToBuyAndSellStock {
 	// Time complexity is O(n).
 	// Space complexity is O(1).
 
-	public int maxProfit2(int[] prices) {
+	public int maxProfit3(int[] prices) {
 		int buy = Integer.MIN_VALUE, sell = 0;
 		for (int i = 0; i < prices.length; i++) {
 			buy = Math.max(buy, -prices[i]);
