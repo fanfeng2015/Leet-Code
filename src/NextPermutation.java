177830d016ceddc805284292977c81d075dd325a
Fan Feng <fanfeng2015@gmail.com>
2023-11-17 14:43:39 -0800

    LeetCode #31

--- src/NextPermutation.java
+++ src/NextPermutation.java
@@ -1,18 +1,27 @@
 // LeetCode #31 (Next Permutation).
 
-// Implement next permutation, which rearranges numbers into the lexicographically 
-// next greater permutation of numbers. If such arrangement is not possible, it must
-// rearrange it as the lowest possible order (ie, sorted in ascending order).
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+// - For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], 
+// [3,1,2], [3,2,1].
 
-// The replacement must be in-place, do not allocate extra memory.
+// The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally,
+// if all the permutations of the array are sorted in one container according to their lexicographical order, then the next 
+// permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, 
+// the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
 
-// Here are some examples. Inputs are in the left-hand column and its corresponding
-// outputs are in the right-hand column.
+// - For example, the next permutation of arr = [1,2,3] is [1,3,2].
+// - Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+// - While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+
+// Given an array of integers nums, find the next permutation of nums.
+
+// The replacement must be in place and use only constant extra memory.
 
 public class NextPermutation {
 
 	public void nextPermutation(int[] nums) {
 		int k = -1;
+		// from the end, find the first number that's going down
 		for (int i = nums.length - 2; i >= 0; i--) {
 			if (nums[i] < nums[i + 1]) {
 				k = i;
@@ -23,6 +32,8 @@ public class NextPermutation {
 			reverse(nums, 0, nums.length - 1);
 			return;
 		}
+		// then from the end, find the first number greater than nums[k]
+		// swap then reverse everything after k
 		for (int i = nums.length - 1; i > k; i--) {
 			if (nums[i] > nums[k]) {
 				swap(nums, k, i);
