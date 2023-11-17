7197e29e03f488d46dbb2b691ce467ec433a39db
Fan Feng <fanfeng2015@gmail.com>
2023-11-17 14:18:44 -0800

    LeetCode #938

--- src/RangeSumOfBST.java
+++ src/RangeSumOfBST.java
@@ -3,6 +3,10 @@
 // Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value
 // in the inclusive range [low, high].
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class RangeSumOfBST {
 
 	public int rangeSumBST(TreeNode root, int low, int high) {
@@ -21,4 +25,27 @@ public class RangeSumOfBST {
 
 	// Time complexity is O(n).
 	// Space complexity is O(n).
+
+	public int rangeSumBST2(TreeNode root, int low, int high) {
+		int sum = 0;
+		LinkedList<TreeNode> stack = new LinkedList<>();
+		TreeNode cur = root;
+		while (cur != null || !stack.isEmpty()) {
+			if (cur != null) {
+				stack.offerFirst(cur);
+				cur = cur.left;
+			} else {
+				cur = stack.pollFirst();
+				if (cur.val > high) {
+					break;
+				}
+				if (cur.val >= low && cur.val <= high) {
+					sum += cur.val;
+				}
+				cur = cur.right;
+			}
+		}
+		return sum;
+	}
+
 }
