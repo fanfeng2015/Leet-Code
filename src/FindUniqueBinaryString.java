f0564910b70ffaa6650ce316c4a288d01869fbb5
Fan Feng <fanfeng2015@gmail.com>
2023-11-18 16:29:21 -0800

    LeetCode #1980

---
+++ src/FindUniqueBinaryString.java
@@ -0,0 +1,19 @@
+// LeetCode #1980 (Find Unique Binary String).
+
+// Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not
+// appear in nums. If there are multiple answers, you may return any of them.
+
+public class FindUniqueBinaryString {
+
+	public String findDifferentBinaryString(String[] nums) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < nums.length; i++) {
+			String str = nums[i];
+			sb.append(str.charAt(i) == '0' ? "1" : "0");
+		}
+		return sb.toString();
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(1).
+}
