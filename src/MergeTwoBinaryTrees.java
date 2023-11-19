9cd7c02d61ba6351ff05cf73c4911235519af488
Fan Feng <fanfeng2015@gmail.com>
2023-11-18 17:40:41 -0800

    LeetCode #367

--- src/MergeTwoBinaryTrees.java
+++ src/MergeTwoBinaryTrees.java
@@ -1,11 +1,14 @@
 // LeetCode #617 (Merge Two Binary Trees).
 
-// Given two binary trees and imagine that when you put one of them to cover the other, 
-// some nodes of the two trees are overlapped while the others are not.
+// You are given two binary trees root1 and root2.
 
-// You need to merge them into a new binary tree. The merge rule is that if two nodes 
-// overlap, then sum node values up as the new value of the merged node. Otherwise, the
-// not null node will be used as the node of new tree.
+// Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. 
+// You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as
+// the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+// Return the merged tree.
+
+// Note: The merging process must start from the root nodes of both trees.
 
 public class MergeTwoBinaryTrees {
 
