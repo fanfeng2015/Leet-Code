3cc3d2395ee7784a3e8cb672d9c21bf2be95f019
Fan Feng <fanfeng2015@gmail.com>
2023-11-23 17:24:21 -0800

    LeetCode #556

--- src/NextGreaterElement3.java
+++ src/NextGreaterElement3.java
@@ -1,9 +1,10 @@
 // LeetCode #556 (Next Greater Element III).
 
-// Given a positive 32-bit integer n, you need to find the smallest 32-bit integer which has
-// exactly the same digits existing in the integer n and is greater in value than n. 
+// Given a positive integer n, find the smallest integer which has exactly the same digits existing in the integer n and
+// is greater in value than n. If no such positive integer exists, return -1.
 
-// If no such positive 32-bit integer exists, you need to return -1.
+// Note that the returned integer should fit in 32-bit integer, if there is a valid answer but it does not fit in 32-bit
+// integer, return -1.
 
 public class NextGreaterElement3 {
 
