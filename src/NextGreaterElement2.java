e5dc250366574bacd493d3d29b8bb2350551bdf9
Fan Feng <fanfeng2015@gmail.com>
2023-11-23 17:16:41 -0800

    LeetCode #503

---
+++ src/NextGreaterElement2.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+
+// LeetCode #503 (Next Greater Element II).
+
+// Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next 
+// greater number for every element in nums.
+
+// The next greater number of a number x is the first greater number to its traversing-order next in the array, which 
+// means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+public class NextGreaterElement2 {
+
+	public int[] nextGreaterElements(int[] nums) {
+		int n = nums.length;
+		LinkedList<Integer> stack = new LinkedList<>(); // [ index ]
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			while (!stack.isEmpty() && nums[stack.peekLast()] < nums[i]) {
+				result[stack.pollLast()] = nums[i];
+			}
+			stack.offerLast(i);
+		}
+		int i = 0;
+		while (!stack.isEmpty() && i < n) {
+			if (nums[stack.peekLast()] < nums[i]) {
+				result[stack.pollLast()] = nums[i];
+			} else {
+				i++;
+			}
+		}
+		while (!stack.isEmpty()) {
+			result[stack.pollLast()] = -1;
+		}
+		return result;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(n).
+}
