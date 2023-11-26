478afc41bb025eaeff38be0248debf58292df593
Fan Feng <fanfeng2015@gmail.com>
2023-11-25 23:43:33 -0800

    LeetCode #639

--- src/DecodeWays2.java
+++ src/DecodeWays2.java
@@ -1,15 +1,25 @@
 // LeetCode #639 (Decode Ways II).
 
-// A message containing letters from A-Z is being encoded to numbers using the following 
-// mapping way: 'A' -> 1, 'B' -> 2, ..., 'Z' -> 26.
+// A message containing letters from A-Z can be encoded into numbers using the following mapping:
+// - 'A' -> "1"
+// - 'B' -> "2"
+// - ...
+// - 'Z' -> "26"
 
-// Beyond that, now the encoded string can also contain the character '*', which can be 
-// treated as one of the numbers from 1 to 9.
+// To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping 
+// above (there may be multiple ways). For example, "11106" can be mapped into:
+// - "AAJF" with the grouping (1 1 10 6)
+// - "KJF" with the grouping (11 10 6)
 
-// Given the encoded message containing digits and the character '*', return the total 
-// number of ways to decode it.
+// Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
 
-// Also, since the answer may be very large, you should return the output mod (10^9 + 7).
+// In addition to the mapping above, an encoded message may contain the '*' character, which can represent any digit from '1'
+// to '9' ('0' is excluded). For example, the encoded message "1*" may represent any of the encoded messages "11", "12", "13",
+// "14", "15", "16", "17", "18", or "19". Decoding "1*" is equivalent to decoding any of the encoded messages it can represent.
+
+// Given a string s consisting of digits and '*' characters, return the number of ways to decode it.
+
+// Since the answer may be very large, return it modulo 109 + 7.
 
 public class DecodeWays2 {
 
