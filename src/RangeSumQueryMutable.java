475afaca7cead21af40f8210f6b7ed422a642133
Fan Feng <fanfeng2015@gmail.com>
2023-11-26 00:14:21 -0800

    LeetCode #307

--- src/RangeSumQueryMutable.java
+++ src/RangeSumQueryMutable.java
@@ -1,15 +1,14 @@
 // LeetCode #307 (Range Sum Query - Mutable).
 
-// Given an integer array nums, find the sum of the elements between indices i 
-// and j (i ≤ j), inclusive.
+// Given an integer array nums, handle multiple queries of the following types:
+// - Update the value of an element in nums.
+// - Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
 
-// The update(i, val) function modifies nums by updating the element at index i 
-// to val.
-
-// Notes:
-// 1. The array is only modifiable by the update function.
-// 2. You may assume the number of calls to update and sumRange function is 
-//    distributed evenly.
+// Implement the NumArray class:
+// - NumArray(int[] nums) Initializes the object with the integer array nums.
+// - void update(int index, int val) Updates the value of nums[index] to be val.
+// - int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right
+//   inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
 
 public class RangeSumQueryMutable {
 
@@ -63,47 +62,40 @@ public class RangeSumQueryMutable {
 	// and to query.
 	// Space complexity is O(n).
 
-/*
-	// Solution 2: Binary indexed tree
-	private int n;
-	private int[] nums;
-	private int[] bit;
-
-	public RangeSumQueryMutable(int[] nums) {
-		this.n = nums.length;
-		this.nums = new int[n];
-		bit = new int[n + 1];
-		for (int i = 0; i < n; i++) {
-			update(i, nums[i]);
-		}
-	}
-
-	public void update(int i, int val) {
-		int diff = val - nums[i];
-		nums[i] = val;
-		i++;
-		while (i <= n) {
-			bit[i] += diff;
-			i += (i & -i); // 1->2, 2->4, 3->4, 4->8, 5->6, 6->8, 7->8, ...
-		}
-	}
-
-	public int sumRange(int i, int j) {
-		return findSum(j) - findSum(i - 1);
-	}
-
-	private int findSum(int i) {
-		int sum = 0;
-		i++;
-		while (i > 0) {
-			sum += bit[i];
-			i -= (i & -i); // 8->0, 7->6, 6->4, 5->4, 4->0, 3->2, 2->0, 1->0, ...
-		}
-		return sum;
-	}
-
-	// Time complexity is O(n*log(n)) to construct the segment tree and O(log(n)) to
-	// update and to query.
-	// Space complexity is O(n).
-*/
+//  Solution 2: Binary indexed tree
+//	private int n;
+//	private int[] nums;
+//	private int[] bit;
+//
+//	public RangeSumQueryMutable(int[] nums) {
+//		this.n = nums.length;
+//		this.nums = new int[n];
+//		bit = new int[n + 1];
+//		for (int i = 0; i < n; i++) {
+//			update(i, nums[i]);
+//		}
+//	}
+//
+//	public void update(int i, int val) {
+//		int diff = val - nums[i];
+//		nums[i++] = val;
+//		while (i <= n) {
+//			bit[i] += diff;
+//			i += (i & -i); // 1->2, 2->4, 3->4, 4->8, 5->6, 6->8, 7->8, ...
+//		}
+//	}
+//
+//	public int sumRange(int i, int j) {
+//		return findSum(j) - findSum(i - 1);
+//	}
+//
+//	private int findSum(int i) {
+//		int sum = 0;
+//		i++;
+//		while (i > 0) {
+//			sum += bit[i];
+//			i -= (i & -i); // 8->0, 7->6, 6->4, 5->4, 4->0, 3->2, 2->0, 1->0, ...
+//		}
+//		return sum;
+//	}
 }
