caa844275c15efa740449371c4e023366952bc6c
Fan Feng <fanfeng2015@gmail.com>
2023-11-26 17:14:54 -0800

    LeetCode #543

--- src/DiameterOfBinaryTree.java
+++ src/DiameterOfBinaryTree.java
@@ -1,68 +1,67 @@
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
 // LeetCode #543 (Diameter of Binary Tree).
 
-// Given a binary tree, you need to compute the length of the diameter of the tree. 
-// The diameter of a binary tree is the length of the longest path between any two 
-// nodes in a tree. This path may or may not pass through the root.
+// Given the root of a binary tree, return the length of the diameter of the tree.
+
+// The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may
+// not pass through the root.
+
+// The length of a path between two nodes is represented by the number of edges between them.
 
 public class DiameterOfBinaryTree {
 
 	private int max = 0;
 
 	public int diameterOfBinaryTree(TreeNode root) {
-		helper(root);
+		longestFromNode(root);
 		return max;
 	}
 
-	// Returns the length of the longest path that starts from the root node.
-	private int helper(TreeNode root) {
-		if (root == null) {
+	private int longestFromNode(TreeNode node) {
+		if (node == null) {
 			return 0;
 		}
-		int left = helper(root.left);
-		int right = helper(root.right);
+		int left = longestFromNode(node.left);
+		int right = longestFromNode(node.right);
 		max = Math.max(max, left + right);
-		return Math.max(left, right) + 1;
+		return 1 + Math.max(left, right);
 	}
 
 	// Time complexity is O(n).
 	// Space complexity is O(n).
 
-/*	
-	// Follow up (Facebook): Print the longest path.
-	private List<Integer> longest = new ArrayList<>();
-
-	public List<Integer> longestPathOfBinaryTree(TreeNode root) {
-		longest(root);
-		return longest;
-	}
-
-	// Returns the longest path that starts from the root node.
-	private List<Integer> longest(TreeNode root) {
-		if (root == null) {
-			return new ArrayList<>();
-		}
-		List<Integer> left = longest(root.left);
-		List<Integer> right = longest(root.right);
-		if (left.size() + right.size() > longest.size()) {
-			longest = new ArrayList<>(left);
-			longest.add(root.val);
-			longest.addAll(reverse(right));
-		}
-		List<Integer> longer = (left.size() < right.size()) ? right : left;
-		longer.add(root.val);
-		return longer;
-	}
-
-	private List<Integer> reverse(List<Integer> list) {
-		List<Integer> result = new ArrayList<>();
-		for (int i = 0; i < list.size(); i++) {
-			result.add(list.get(list.size() - 1 - i));
-		}
-		return result;
-	}
-*/
-
+//	Follow up (Facebook): Print the longest path.
+//	private List<Integer> longest = new ArrayList<>();
+//
+//	public List<Integer> longestPathOfBinaryTree(TreeNode root) {
+//		longest(root);
+//		return longest;
+//	}
+//
+//	// Returns the longest path that starts from the root node.
+//	private List<Integer> longest(TreeNode root) {
+//		if (root == null) {
+//			return new ArrayList<>();
+//		}
+//		List<Integer> left = longest(root.left);
+//		List<Integer> right = longest(root.right);
+//		if (left.size() + right.size() > longest.size()) {
+//			longest = new ArrayList<>(left);
+//			longest.add(root.val);
+//			longest.addAll(reverse(right));
+//		}
+//		List<Integer> longer = (left.size() < right.size()) ? right : left;
+//		longer.add(root.val);
+//		return longer;
+//	}
+//
+//	private List<Integer> reverse(List<Integer> list) {
+//		List<Integer> result = new ArrayList<>();
+//		for (int i = 0; i < list.size(); i++) {
+//			result.add(list.get(list.size() - 1 - i));
+//		}
+//		return result;
+//	}
 }
