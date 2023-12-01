f7aeddc01b880854b28e0655eea05dd6e600dee2
Fan Feng <fanfeng2015@gmail.com>
2023-12-01 00:18:45 -0800

    LeetCode #173

--- src/BinarySearchTreeIterator.java
+++ src/BinarySearchTreeIterator.java
@@ -2,42 +2,42 @@ import java.util.LinkedList;
 
 // LeetCode #173 (Binary Search Tree Iterator).
 
-// Implement an iterator over a binary search tree (BST). Your iterator will be initialized
-// with the root node of a BST.
+// Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+// - BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the
+//   constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+// - boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+// - int next() Moves the pointer to the right, then returns the number at the pointer.
 
-// Calling next() will return the next smallest number in the BST.
+// Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest 
+// element in the BST.
 
-// Note: 
-// next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the
-// height of the tree.
+// You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal 
+// when next() is called.
 
 public class BinarySearchTreeIterator {
 
-	TreeNode cur;
-	LinkedList<TreeNode> stack;
+	LinkedList<TreeNode> stack = new LinkedList<>();
 
 	public BinarySearchTreeIterator(TreeNode root) {
-		cur = root;
-		stack = new LinkedList<TreeNode>();
+		TreeNode node = root;
+		while (node != null) {
+			stack.offerLast(node);
+			node = node.left;
+		}
+	}
+
+	public int next() {
+		TreeNode node = stack.pollLast();
+		TreeNode cur = node.right;
 		while (cur != null) {
 			stack.offerLast(cur);
 			cur = cur.left;
 		}
+		return node.val;
 	}
 
 	public boolean hasNext() {
 		return stack.size() > 0;
-	}
-
-	public int next() {
-		int result = stack.peekLast().val;
-		cur = stack.pollLast();
-		cur = cur.right;
-		while (cur != null) {
-			stack.offerLast(cur);
-			cur = cur.left;
-		}
-		return result;
 	}
 
 	// Time complexity is O(1) for hasNext() and O(h) for next().
@@ -46,42 +46,41 @@ public class BinarySearchTreeIterator {
 	// Follow up (Facebook):
 	// 1. Two BSTs.
 	// 2. O(1) space -> Morris.
-	
-/*
-	public BinarySearchTreeIterator(TreeNode root) {
-		cur = root;
-	}
 
-	public boolean hasNext() {
-		return cur != null;
-	}
-
-	public int next() {
-		TreeNode temp = null;
-		int result = 0;
-		while (cur != null) {
-			if (cur.left == null) {
-				result = cur.val;
-				cur = cur.right;
-				break; // when next() is called again, while loop continues
-			} else {
-				temp = cur.left;
-				while (temp.right != null && temp.right != cur) {
-					temp = temp.right;
-				}
-				if (temp.right == null) {
-					temp.right = cur;
-					cur = cur.left;
-				} else {
-					result = cur.val;
-					temp.right = null;
-					cur = cur.right;
-					break; // when next() is called again, while loop continues
-				}
-			}
-		}
-		return result;
-	}
-*/
-
+//	TreeNode cur;
+//
+//	public BinarySearchTreeIterator(TreeNode root) {
+//		cur = root;
+//	}
+//
+//	public boolean hasNext() {
+//		return cur != null;
+//	}
+//
+//	public int next() {
+//		TreeNode temp = null;
+//		int result = 0;
+//		while (cur != null) {
+//			if (cur.left == null) {
+//				result = cur.val;
+//				cur = cur.right;
+//				break; // when next() is called again, while loop continues
+//			} else {
+//				temp = cur.left;
+//				while (temp.right != null && temp.right != cur) {
+//					temp = temp.right;
+//				}
+//				if (temp.right == null) {
+//					temp.right = cur;
+//					cur = cur.left;
+//				} else {
+//					result = cur.val;
+//					temp.right = null;
+//					cur = cur.right;
+//					break; // when next() is called again, while loop continues
+//				}
+//			}
+//		}
+//		return result;
+//	}
 }
