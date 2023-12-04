98869a3111521b8372d444bdf9cee2e5c3d0bac5
Fan Feng <fanfeng2015@gmail.com>
2023-12-03 17:22:04 -0800

    LeetCode #341

--- src/FlattenNestedListIterator.java
+++ src/FlattenNestedListIterator.java
@@ -1,44 +1,54 @@
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 // LeetCode #341 (Flatten Nested List Iterator).
 
-// Given a nested list of integers, implement an iterator to flatten it.
+// You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be 
+// integers or other lists. Implement an iterator to flatten it.
 
-// Each element is either an integer, or a list -- whose elements may also
-// be integers or other lists.
+// Implement the NestedIterator class:
+// - NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
+// - int next() Returns the next integer in the nested list.
+// - boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+
+// Your code will be tested with the following pseudocode:
+//   initialize iterator with nestedList
+//   res = []
+//   while iterator.hasNext()
+//     append iterator.next() to the end of res
+//   return res
+
+// If res matches the expected flattened list, then your code will be judged as correct.
 
 public class FlattenNestedListIterator implements Iterator<Integer> {
 
-	Stack<NestedInteger> stack = new Stack<>();
+	LinkedList<NestedInteger> stack = new LinkedList<>();
 
 	public FlattenNestedListIterator(List<NestedInteger> nestedList) {
 		for (int i = nestedList.size() - 1; i >= 0; i--) {
-			stack.push(nestedList.get(i));
+			stack.offerLast(nestedList.get(i));
 		}
 	}
 
 	@Override
 	public Integer next() {
-		return stack.pop().getInteger();
+		return stack.pollLast().getInteger();
 	}
 
 	@Override
 	public boolean hasNext() {
 		while (!stack.isEmpty()) {
-			NestedInteger cur = stack.peek();
+			NestedInteger cur = stack.peekLast();
 			if (cur.isInteger()) {
 				return true;
 			} else {
-				stack.pop();
-				List<NestedInteger> list = cur.getList();
-				for (int i = list.size() - 1; i >= 0; i--) {
-					stack.push(list.get(i));
+				stack.pollLast();
+				for (int i = cur.getList().size() - 1; i >= 0; i--) {
+					stack.offerLast(cur.getList().get(i));
 				}
 			}
 		}
 		return false;
 	}
-
 }
