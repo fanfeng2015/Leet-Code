d9937d5d1802d8bba571f99760c79865a2654292
Fan Feng <fanfeng2015@gmail.com>
2023-12-09 22:26:11 -0800

    LeetCode #65

--- src/ValidNumber.java
+++ src/ValidNumber.java
@@ -1,6 +1,25 @@
 // LeetCode #65 (Valid Number).
 
-// Validate if a given string is numeric.
+// A valid number can be split up into these components (in order):
+// - A decimal number or an integer.
+// - (Optional) An 'e' or 'E', followed by an integer.
+
+// A decimal number can be split up into these components (in order):
+// - (Optional) A sign character (either '+' or '-').
+// - One of the following formats:
+//   - One or more digits, followed by a dot '.'.
+//   - One or more digits, followed by a dot '.', followed by one or more digits.
+//   - A dot '.', followed by one or more digits.
+
+// An integer can be split up into these components (in order):
+// - (Optional) A sign character (either '+' or '-').
+// - One or more digits.
+
+// For example, all the following are valid numbers: ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", 
+// "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", 
+// "95a54e53"].
+
+// Given a string s, return true if s is a valid number.
 
 public class ValidNumber {
 
@@ -17,7 +36,7 @@ public class ValidNumber {
 					return false;
 				}
 				point = true;
-			} else if (s.charAt(i) == 'e') { // e can't be processed by e or non-digit
+			} else if (s.charAt(i) == 'e' || s.charAt(i) == 'E') { // e can't be proceeded by e or non-digit
 				if (e || !number) {
 					return false;
 				}
@@ -25,7 +44,7 @@ public class ValidNumber {
 				numberAfterE = false;
 			} else if (s.charAt(i) == '+' || s.charAt(i) == '-') {
 				// either the first char, or the previous char is e
-				if (i != 0 && s.charAt(i - 1) != 'e') {
+				if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
 					return false;
 				}
 			} else {
