326e787a9bde79824810afe5631f34bd2359fd66
Fan Feng <fanfeng2015@gmail.com>
2023-12-13 23:31:46 -0800

    LeetCode #415

--- src/AddStrings.java
+++ src/AddStrings.java
@@ -1,7 +1,9 @@
 // LeetCode #415 (Add Strings).
 
-// Given two non-negative integers num1 and num2 represented as string, return
-// the sum of num1 and num2.
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not
+// convert the inputs to integers directly.
 
 public class AddStrings {
 
