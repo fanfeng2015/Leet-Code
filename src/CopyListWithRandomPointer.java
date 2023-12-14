bd8dddcf608028fbfe8085609400b63a807ca555
Fan Feng <fanfeng2015@gmail.com>
2023-12-13 22:23:29 -0800

    LeetCode #138

--- src/CopyListWithRandomPointer.java
+++ src/CopyListWithRandomPointer.java
@@ -3,32 +3,60 @@ import java.util.Map;
 
 // LeetCode #138 (Copy List with Random Pointer).
 
-// A linked list is given such that each node contains an additional random pointer which
-// could point to any node in the list or null.
+// A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in
+// the list, or null.
 
-// Return a deep copy of the list.
+// Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value
+// set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes 
+// in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers
+// in the new list should point to nodes in the original list.
+
+// For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x 
+// and y in the copied list, x.random --> y.
+
+// Return the head of the copied linked list.
+
+// The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index]
+// where:
+// - val: an integer representing Node.val
+// - random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any
+//   node.
+
+// Your code will only be given the head of the original linked list.
 
 public class CopyListWithRandomPointer {
 
-	public RandomListNode copyRandomList(RandomListNode head) {
+	private class Node {
+		int val;
+		Node next;
+		Node random;
+
+		public Node(int val) {
+			this.val = val;
+			this.next = null;
+			this.random = null;
+		}
+	}
+
+	public Node copyRandomList(Node head) {
 		if (head == null) {
 			return null;
 		}
-		Map<RandomListNode, RandomListNode> lookUp = new HashMap<>();
-		RandomListNode newHead = new RandomListNode(head.label);
-		RandomListNode cur = newHead;
+		Map<Node, Node> lookUp = new HashMap<>();
+		Node newHead = new Node(head.val);
+		Node cur = newHead;
 		lookUp.put(head, newHead);
 		while (head != null) {
 			if (head.next != null) {
 				if (!lookUp.containsKey(head.next)) {
-					RandomListNode node = new RandomListNode(head.next.label);
+					Node node = new Node(head.next.val);
 					lookUp.put(head.next, node);
 				}
 				cur.next = lookUp.get(head.next);
 			}
 			if (head.random != null) {
 				if (!lookUp.containsKey(head.random)) {
-					RandomListNode node = new RandomListNode(head.random.label);
+					Node node = new Node(head.random.val);
 					lookUp.put(head.random, node);
 				}
 				cur.random = lookUp.get(head.random);
@@ -43,14 +71,14 @@ public class CopyListWithRandomPointer {
 	// Space complexity is O(n).
 
 	// Follow up: Use O(1) space?
-	public RandomListNode copyRandomList2(RandomListNode head) {
+	public Node copyRandomList2(Node head) {
 		if (head == null) {
 			return null;
 		}
-		RandomListNode cur = head;
+		Node cur = head;
 		while (cur != null) {
-			RandomListNode node = cur.next;
-			RandomListNode newNext = new RandomListNode(cur.label);
+			Node node = cur.next;
+			Node newNext = new Node(cur.val);
 			cur.next = newNext;
 			newNext.next = node;
 			cur = cur.next.next;
@@ -61,9 +89,9 @@ public class CopyListWithRandomPointer {
 			cur = cur.next.next;
 		}
 		cur = head;
-		RandomListNode newHead = head.next;
+		Node newHead = head.next;
 		while (cur != null) {
-			RandomListNode prev = cur.next.next; // possibly null
+			Node prev = cur.next.next; // possibly null
 			cur.next.next = (prev == null) ? null : prev.next;
 			cur.next = prev;
 			cur = cur.next;
