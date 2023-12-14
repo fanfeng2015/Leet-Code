8326c0bed406e0307b5a8f250899641251262dc4
Fan Feng <fanfeng2015@gmail.com>
2023-12-13 23:26:53 -0800

    LeetCode #346

--- src/MovingAverageFromDataStream.java
+++ src/MovingAverageFromDataStream.java
@@ -1,7 +1,10 @@
 // LeetCode #346 (Moving Average from Data Stream).
 
-// Given a stream of integers and a window size, calculate the moving average of all
-// integers in the sliding window.
+// Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+// Implement the MovingAverage class:
+// - MovingAverage(int size) Initializes the object with the size of the window size.
+// - double next(int val) Returns the moving average of the last size values of the stream.
 
 public class MovingAverageFromDataStream {
 
