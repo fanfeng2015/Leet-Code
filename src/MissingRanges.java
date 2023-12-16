ca13e3b11998cac5629233afd299edb039f13b6b
Fan Feng <fanfeng2015@gmail.com>
2023-12-15 21:34:42 -0800

    LeetCode #163

--- src/MissingRanges.java
+++ src/MissingRanges.java
@@ -3,12 +3,35 @@ import java.util.List;
 
 // LeetCode #163 (Missing Ranges).
 
-// Given a sorted integer array nums, where the range of elements are in the
-// inclusive range [lower, upper], return its missing ranges.
+// You are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are within the inclusive
+// range.
+
+// A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+
+// Return the shortest sorted list of ranges that exactly covers all the missing numbers. That is, no element of nums is included in 
+// any of the ranges, and each missing number is covered by one of the ranges.
 
 public class MissingRanges {
 
-	public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+	// ------------------------------ 2023 ------------------------------
+	public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
+		List<List<Integer>> result = new ArrayList<>();
+		int prev = lower - 1;
+		for (int i = 0; i <= nums.length; i++) {
+			int cur = (i == nums.length) ? upper + 1 : nums[i];
+			if (cur - prev >= 2) {
+				List<Integer> list = new ArrayList<>();
+				list.add(prev + 1);
+				list.add(cur - 1);
+				result.add(list);
+			}
+			prev = cur;
+		}
+		return result;
+	}
+
+	// ------------------------------ 2018 ------------------------------
+	List<String> findMissingRanges2(int[] nums, int lower, int upper) {
 		List<String> result = new ArrayList<>();
 		long prev = (long) lower - 1;
 		for (int i = 0; i <= nums.length; i++) {
