9e8c64675fd731dd175556c5859ade2e0a9f9a5e
Fan Feng <fanfeng2015@gmail.com>
2023-12-15 21:41:01 -0800

    LeetCode #348

--- src/DesignTicTacToe.java
+++ src/DesignTicTacToe.java
@@ -1,12 +1,17 @@
 // LeetCode #348 (Design Tic-Tac-Toe).
 
-// Design a Tic-tac-toe game that is played between two players on a n x n grid.
+// Assume the following rules are for the tic-tac-toe game on an n x n board between two players:
+// - A move is guaranteed to be valid and is placed on an empty block.
+// - Once a winning condition is reached, no more moves are allowed.
+// - A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
 
-// You may assume the following rules:
-// 1. A move is guaranteed to be valid and is placed on an empty block.
-// 2. Once a winning condition is reached, no more moves is allowed.
-// 3. A player who succeeds in placing n of their marks in a horizontal, vertical,
-//    or diagonal row wins the game.
+// Implement the TicTacToe class:
+// - TicTacToe(int n) Initializes the object the size of the board n.
+// - int move(int row, int col, int player) Indicates that the player with id player plays at the cell (row, col) of the board. The 
+//   move is guaranteed to be a valid move, and the two players alternate in making moves. Return
+//   - 0 if there is no winner after the move,
+//   - 1 if player 1 is the winner after the move, or
+//   - 2 if player 2 is the winner after the move.
 
 public class DesignTicTacToe {
 
