1edafd1e91ce86ce49ad35772f1d16dc661dc305
Fan Feng <fanfeng2015@gmail.com>
2023-12-17 18:27:33 -0800

    LeetCode #317

--- src/ShortestDistanceFromAllBuildings.java
+++ src/ShortestDistanceFromAllBuildings.java
@@ -2,17 +2,20 @@ import java.util.LinkedList;
 
 // LeetCode #317 (Shortest Distance from All Buildings).
 
-// You want to build a house on an empty land which reaches all buildings in the shortest
-// amount of distance. You can only move up, down, left and right. You are given a 2D grid
-// of values 0, 1 or 2, where:
+// You are given an m x n grid grid of values 0, 1, or 2, where:
+// - each 0 marks an empty land that you can pass by freely,
+// - each 1 marks a building that you cannot pass through, and
+// - each 2 marks an obstacle that you cannot pass through.
 
-// Each 0 marks an empty land which you can pass by freely.
-// Each 1 marks a building which you cannot pass through.
-// Each 2 marks an obstacle which you cannot pass through.
+// You want to build a house on an empty land that reaches all buildings in the shortest total travel distance. You can only move 
+// up, down, left, and right.
 
-// Note: 
-// There will be at least one building. If it is not possible to build such house according
-// to the above rules, return -1.
+// Return the shortest travel distance for such a house. If it is not possible to build such a house according to the above rules, 
+// return -1.
+
+// The total travel distance is the sum of the distances between the houses of the friends and the meeting point.
+
+// The distance is calculated using Manhattan Distance, where distance(p1, p2) = |p2.x - p1.x| + |p2.y - p1.y|.
 
 public class ShortestDistanceFromAllBuildings {
 
