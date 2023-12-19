512d573252e5c33ffe33c967cd842a257c6529bb
Fan Feng <fanfeng2015@gmail.com>
2023-12-18 22:01:07 -0800

    LeetCode #269

--- src/AlienDictionary.java
+++ src/AlienDictionary.java
@@ -7,11 +7,15 @@ import java.util.Set;
 
 // LeetCode #269 (Alien Dictionary).
 
-// There is a new alien language which uses the latin alphabet. However, the order
-// among letters are unknown to you. You receive a list of non-empty words from the
-// dictionary, where words are sorted lexicographically by the rules of this new language. 
+// There is a new alien language that uses the English alphabet. However, the order of the letters is unknown to you.
 
-// Derive the order of letters in this language.
+// You are given a list of strings words from the alien language's dictionary. Now it is claimed that the strings in words are 
+// sorted lexicographically by the rules of this new language.
+
+// If this claim is incorrect, and the given arrangement of string in words cannot correspond to any order of letters, return "".
+
+// Otherwise, return a string of the unique letters in the new alien language sorted in lexicographically increasing order by the
+// new language's rules. If there are multiple solutions, return any of them.
 
 public class AlienDictionary {
 
