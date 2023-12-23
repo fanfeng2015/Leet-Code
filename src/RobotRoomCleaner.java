09d1f3ab3e14b655c1db45092bea77dc4e476253
Fan Feng <fanfeng2015@gmail.com>
2023-12-22 23:46:48 -0800

    LeetCode #489

--- src/RobotRoomCleaner.java
+++ src/RobotRoomCleaner.java
@@ -3,26 +3,39 @@ import java.util.Set;
 
 // LeetCode #489 (Robot Room Cleaner).
 
-// Given a robot cleaner in a room modeled as a grid.
+// You are controlling a robot that is located somewhere in a room. The room is modeled as an m x n binary grid where 0 represents a 
+// wall and 1 represents an empty slot.
 
-// Each cell in the grid can be empty or blocked.
+// The robot starts at an unknown location in the room that is guaranteed to be empty, and you do not have access to the grid, but you
+// can move the robot using the given API Robot.
 
-// The robot cleaner with 4 given APIs can move forward, turn left or turn right. 
-// Each turn it made is 90 degrees.
+// You are tasked to use the robot to clean the entire room (i.e., clean every empty cell in the room). The robot with the four given
+// APIs can move forward, turn left, or turn right. Each turn is 90 degrees.
 
-// When it tries to move into a blocked cell, its bumper sensor detects the obstacle
-// and it stays on the current cell.
+// When the robot tries to move into a wall cell, its bumper sensor detects the obstacle, and it stays on the current cell.
 
-// Notes:
-// 1. The input is only given to initialize the room and the robot's position internally. 
-//    You must solve this problem "blindfolded". In other words, you must control the robot
-//    using only the mentioned 4 APIs, without knowing the room layout and the initial robot's
-//    position.
-// 2. The robot's initial position will always be in an accessible cell.
-// 3. The initial direction of the robot will be facing up.
-// 4. All accessible cells are connected, which means the all cells marked as 1 will be accessible
-//    by the robot.
-// 5. Assume all four edges of the grid are all surrounded by wall.
+// Design an algorithm to clean the entire room using the following APIs:
+//   interface Robot {
+//     // returns true if next cell is open and robot moves into the cell.
+//     // returns false if next cell is obstacle and robot stays on the current cell.
+//     boolean move();
+//
+//     // Robot will stay on the same cell after calling turnLeft/turnRight.
+//     // Each turn will be 90 degrees.
+//     void turnLeft();
+//     void turnRight();
+//
+//     // Clean the current cell.
+//     void clean();
+//   }
+
+// Note that the initial direction of the robot will be facing up. You can assume all four edges of the grid are all surrounded by a
+// wall.
+
+// Custom testing:
+// The input is only given to initialize the room and the robot's position internally. You must solve this problem "blindfolded". In
+// other words, you must control the robot using only the four mentioned APIs without knowing the room layout and the initial robot's
+// position.
 
 public class RobotRoomCleaner {
 
